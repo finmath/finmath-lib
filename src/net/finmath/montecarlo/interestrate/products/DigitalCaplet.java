c910a8dc10121979ae368e277c25123d7e99aaba
Christian Fries <email@christian-fries.de>
2013-06-07 20:22:28 +0000

    Added Monte-Carlo valuation of a digital caplet.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@448 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/net/finmath/montecarlo/interestrate/products/DigitalCaplet.java
@@ -0,0 +1,88 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ *
+ * Created on 06.04.2005
+ */
+package net.finmath.montecarlo.interestrate.products;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.montecarlo.RandomVariable;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+import net.finmath.stochastic.ImmutableRandomVariableInterface;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * Implements the valuation of a digital caplet using a given
+ * <code>LIBORModelMonteCarloSimulationInterface</code>.
+ * The digital caplet pays periodLength if <i>L &gt; K</i> and else 0.
+ * Here <i>L = L(T<sub>i</sub>,T<sub>i+1</sub>;t)</i> is the
+ * forward rate with period start <i>T<sub>i</sub></i>
+ * and period end <i>T<sub>i+1</sub></i> and fixing <i>t</i>.
+ * <i>K</i> denotes the strike rate.
+ * 
+ * @author Christian Fries
+ * @version 1.1
+ */
+public class DigitalCaplet extends AbstractLIBORMonteCarloProduct {
+	private double	optionMaturity;
+	private double	periodStart;
+	private double	periodEnd;
+	private double	strike;
+		
+	/**
+	 * Create a digital caplet with given maturity and strike.
+	 * 
+	 * @param optionMaturity The option maturity.
+	 * @param periodStart The period start of the forward rate.
+	 * @param periodEnd The period end of the forward rate.
+	 * @param strike The strike rate.
+	 */
+	public DigitalCaplet(double optionMaturity, double periodStart,
+			double periodEnd, double strike) {
+		super();
+		this.optionMaturity = optionMaturity;
+		this.periodStart = periodStart;
+		this.periodEnd = periodEnd;
+		this.strike = strike;
+	}
+
+    /**
+     * This method returns the value random variable of the product within the specified model, evaluated at a given evalutationTime.
+     * Note: For a lattice this is often the value conditional to evalutationTime, for a Monte-Carlo simulation this is the (sum of) value discounted to evaluation time.
+     * Cashflows prior evaluationTime are not considered.
+     * 
+     * @param evaluationTime The time on which this products value should be observed.
+     * @param model The model used to price the product.
+     * @return The random variable representing the value of the product discounted to evaluation time
+     * @throws CalculationException 
+     */
+    @Override
+    public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {        
+
+		// Set payment date and period length
+		double	paymentDate		= periodEnd;
+		double	periodLength	= periodEnd - periodStart;
+
+        // Get random variables
+		ImmutableRandomVariableInterface	libor		= model.getLIBOR(optionMaturity, periodStart, periodEnd);
+
+		RandomVariableInterface 			trigger		= libor.getMutableCopy().sub(strike).mult(periodLength);
+		RandomVariableInterface				values		= (new RandomVariable(1.0)).barrier(trigger, (new RandomVariable(periodLength)), (new RandomVariable(0.0)));
+
+        // Get numeraire and probabilities for payment time
+		ImmutableRandomVariableInterface	numeraire					= model.getNumeraire(paymentDate);
+		ImmutableRandomVariableInterface	monteCarloProbabilities		= model.getMonteCarloWeights(paymentDate);
+
+		values.div(numeraire).mult(monteCarloProbabilities);
+		
+        // Get numeraire and probabilities for evaluation time
+		ImmutableRandomVariableInterface	numeraireAtEvaluationTime					= model.getNumeraire(evaluationTime);
+		ImmutableRandomVariableInterface	monteCarloProbabilitiesAtEvaluationTime		= model.getMonteCarloWeights(evaluationTime);
+
+		values.mult(numeraireAtEvaluationTime).div(monteCarloProbabilitiesAtEvaluationTime);		
+		
+		// Return values
+		return values;
+	}
+}
+
