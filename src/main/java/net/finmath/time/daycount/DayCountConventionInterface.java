82b0f736772753030a957dbad1ac021d70e6890c
Christian Fries <email@christian-fries.de>
2013-09-07 22:23:41 +0000

    Updated JavaDoc.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@554 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/main/java/net/finmath/time/daycount/DayCountConventionInterface.java
+++ src/main/java/net/finmath/time/daycount/DayCountConventionInterface.java
@@ -9,12 +9,31 @@ package net.finmath.time.daycount;
 import java.util.GregorianCalendar;
 
 /**
+ * Interface implemented by various daycount conventions.
+ * Classes implementing this interface provide daycounting and daycount fractions for
+ * a given interval on a {@link java.util.GregorianCalendar}.
+ * 
  * @author Christian Fries
- *
  */
 public interface DayCountConventionInterface {
 
+	/**
+	 * Return the number of days between startDate and endDate given the
+	 * specific daycount convention.
+	 * 
+	 * @param startDate The start date given as a {@link java.util.GregorianCalendar}.
+	 * @param endDate The end date given as a {@link java.util.GregorianCalendar}.
+	 * @return The number of days within the given period.
+	 */
 	public abstract double getDaycount(GregorianCalendar startDate, GregorianCalendar endDate);
-	public abstract double getDaycountFraction(GregorianCalendar startDate, GregorianCalendar endDate);
 
+	/**
+	 * Return the daycount fraction corresponding to the period from startDate to endDate given the
+	 * specific daycount convention.
+	 * 
+	 * @param startDate The start date given as a {@link java.util.GregorianCalendar}.
+	 * @param endDate The end date given as a {@link java.util.GregorianCalendar}.
+	 * @return The daycount fraction corresponding to the given period.
+	 */
+	public abstract double getDaycountFraction(GregorianCalendar startDate, GregorianCalendar endDate);
 }
