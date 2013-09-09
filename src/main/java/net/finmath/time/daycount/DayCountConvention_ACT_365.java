c27af6b10a8020e1a9b9b493832da4b332669f72
Christian Fries <email@christian-fries.de>
2013-09-09 18:08:26 +0000

    Added ACT/360 and ACT/365 daycount convention.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@558 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/time/daycount/DayCountConvention_ACT_365.java
@@ -0,0 +1,42 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 07.09.2013
+ */
+
+package net.finmath.time.daycount;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Calculates the day count by calculating the actual number of days between startDate and endDate.
+ * 
+ * The method is only exact, if the two calendar dates are (approximately) on the same time. A fractional day is
+ * rounded to the approximately nearest day (since daylight saving is not considered, the notion of nearest may be off by one hour).
+ * 
+ * The day count fraction is calculated using ACT/365 convention, that is, the
+ * day count divided by 365.
+ * 
+ * @author Christian Fries
+ */
+public class DayCountConvention_ACT_365 extends DayCountConvention_ACT implements DayCountConventionInterface {
+
+	/**
+	 * Create an ACT/365 day count convention.
+	 */
+	public DayCountConvention_ACT_365() {
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see net.finmath.time.daycount.DayCountConventionInterface#getDaycountFraction(java.util.GregorianCalendar, java.util.GregorianCalendar)
+	 */
+	@Override
+	public double getDaycountFraction(GregorianCalendar startDate, GregorianCalendar endDate) {
+		if(startDate.after(endDate)) return -getDaycountFraction(endDate,startDate);
+
+		double daycountFraction = getDaycount(startDate, endDate) / 365.0;
+
+		return daycountFraction;
+	}
+}
