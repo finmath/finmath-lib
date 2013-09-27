0ffad0b38bc0ce9cdc50e56da19a46844641d9a3
Christian Fries <email@christian-fries.de>
2013-09-27 22:18:08 +0000

    Updated JavaDoc.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@610 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/main/java/net/finmath/time/daycount/DayCountConvention_ACT.java
+++ src/main/java/net/finmath/time/daycount/DayCountConvention_ACT.java
@@ -36,16 +36,26 @@ public abstract class DayCountConvention_ACT implements DayCountConventionInterf
 	
 	/**
 	 * Returns the number of days, between two Calendar dates.
+	 * 
 	 * The method is only exact, if the two calendar dates are (approximately) on the same time. A fractional day is
 	 * rounded to the approximately nearest day (since daylight saving is not considered, the notion of nearest may be off by one hour).
 	 * 
+	 * The formula implemented is
+	 * <code>
+	 * Math.round( ((double)(endDate.getTimeInMillis()-startDate.getTimeInMillis())) / 1000.0 / 60.0 / 60.0 / 24)
+	 * </code>
+	 * 
+	 * <p>
 	 * For more accurate calculation consider org.joda.time.Days.
+	 * </p>
 	 * 
+	 * <p>
 	 * However, the method is correct if each calendar is on the same time and they differ only by days.
+	 * </p>
 	 * 
-	 * @param startDate
-	 * @param endDate
-	 * @return
+	 * @param startDate The start date of the interval.
+	 * @param endDate The end date of the interval.
+	 * @return Number of days between startDate and endDate.
 	 */
 	private static double daysBetween(Calendar startDate, Calendar endDate) {
 		return Math.round( ((double)(endDate.getTimeInMillis()-startDate.getTimeInMillis())) / 1000.0 / 60.0 / 60.0 / 24);
