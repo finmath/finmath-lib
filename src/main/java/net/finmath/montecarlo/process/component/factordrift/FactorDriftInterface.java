9ebc302107291cf9e3691c1855bc3320e2f51df0
Christian Fries <email@christian-fries.de>
2013-10-06 13:15:34 +0000

    Improved JavaDoc.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@674 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/main/java/net/finmath/montecarlo/process/component/factordrift/FactorDriftInterface.java
+++ src/main/java/net/finmath/montecarlo/process/component/factordrift/FactorDriftInterface.java
@@ -9,8 +9,8 @@ public interface FactorDriftInterface {
 	 * The interface describes how an additional factor scaling may be specified for the generation of a process (see e.g. LogNormalProcess).
 	 * The factor scaling may be used to implement importance sampling or proxy simulation on the level of the discrete process.
 	 * 
-	 * @param timeIndex
-	 * @param realizationPredictor
+	 * @param timeIndex The time index (associated with the process time discretization).
+	 * @param realizationPredictor The realization predictor (in case we use a predictor corrector scheme).
 	 * @return
 	 */
 	RandomVariableInterface[]	getFactorScaling(int timeIndex, RandomVariableInterface[] realizationPredictor);
@@ -19,8 +19,8 @@ public interface FactorDriftInterface {
 	 * The interface describes how an additional factor drift may be specified for the generation of a process (see e.g. LogNormalProcess).
 	 * The factor drift may be used to implement importance sampling or proxy simulation on the level of the discrete process.
 	 * 
-	 * @param timeIndex
-	 * @param realizationPredictor
+	 * @param timeIndex The time index (associated with the process time discretization).
+	 * @param realizationPredictor The realization predictor (in case we use a predictor corrector scheme).
 	 * @return A vector of random variables given the factor drift for each factor. If the size is less then the number of factors, then higher order factors have no drift.
 	 * @throws net.finmath.exception.CalculationException Thrown if the valuation fails, specific cause may be available via the <code>cause()</code> method. 
 	 */
@@ -30,9 +30,9 @@ public interface FactorDriftInterface {
      * The interface describes how an additional factor drift may be specified for the generation of a process (see e.g. LogNormalProcess).
      * The factor drift may be used to implement importance sampling or proxy simulation on the level of the discrete process.
      * 
-     * @param timeIndex
-     * @param realizationPredictor
-     * @return
+	 * @param timeIndex The time index (associated with the process time discretization).
+	 * @param realizationPredictor The realization predictor (in case we use a predictor corrector scheme).
+     * @return The determinant of the factor drift.
      */
 	RandomVariableInterface    getFactorDriftDeterminant(int timeIndex, RandomVariableInterface[] realizationPredictor);
 }
