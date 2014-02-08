2eef2d8f09bc9315f09a6e9f5fdfd6cc3d00238f
Christian Fries <email@christian-fries.de>
2014-02-08 23:22:10 +0000

    Added a factory for random variables (abstraction of the creation of object implementing RandomVariableInterfaces).
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@827 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/montecarlo/AbstractRandomVariableFactory.java
@@ -0,0 +1,23 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ *
+ * Created on 09.02.2004
+ */
+package net.finmath.montecarlo;
+
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ *
+ * @author Christian Fries
+ */
+public abstract class AbstractRandomVariableFactory {
+
+    public RandomVariableInterface createRandomVariable(double value) {
+        return createRandomVariable(-Double.MAX_VALUE, value);
+    }
+
+    public abstract RandomVariableInterface createRandomVariable(double time, double value);
+
+    public abstract RandomVariableInterface createRandomVariable(double time, double[] values);
+}
