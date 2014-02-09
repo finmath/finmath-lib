a87962047229c4cfc95839ca9a14cf5c20a77fd0
Christian Fries <email@christian-fries.de>
2014-02-09 18:44:45 +0000

    Organized imports.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@857 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/main/java/net/finmath/montecarlo/assetderivativevaluation/MonteCarloBlackScholesModel.java
+++ src/main/java/net/finmath/montecarlo/assetderivativevaluation/MonteCarloBlackScholesModel.java
@@ -12,7 +12,6 @@ import net.finmath.exception.CalculationException;
 import net.finmath.montecarlo.BrownianMotion;
 import net.finmath.montecarlo.BrownianMotionInterface;
 import net.finmath.montecarlo.MonteCarloSimulationInterface;
-import net.finmath.montecarlo.RandomVariable;
 import net.finmath.montecarlo.model.AbstractModel;
 import net.finmath.montecarlo.process.AbstractProcess;
 import net.finmath.montecarlo.process.ProcessEulerScheme;
