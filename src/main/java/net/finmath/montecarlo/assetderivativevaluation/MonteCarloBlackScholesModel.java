f977e70ea67084efab2c25c588918107492af276
Christian Fries <email@christian-fries.de>
2014-02-09 09:11:25 +0000

    Removed dependence on specific RandomVariableInterface implementation, where possible.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@830 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/main/java/net/finmath/montecarlo/assetderivativevaluation/MonteCarloBlackScholesModel.java
+++ src/main/java/net/finmath/montecarlo/assetderivativevaluation/MonteCarloBlackScholesModel.java
@@ -102,9 +102,9 @@ public class MonteCarloBlackScholesModel extends AbstractModel implements AssetM
 		 * The interface definition requires that we provide the drift and the volatility in terms of random variables.
 		 * We construct the corresponding random variables here and will return (immutable) references to them.
 		 */
-		this.initialValueVector[0]	= new RandomVariable(0.0, Math.log(initialValue));
-		this.drift					= new RandomVariable(0.0, riskFreeRate - 0.5 * volatility*volatility);
-		this.volatilityOnPaths		= new RandomVariable(0.0, volatility);
+		this.initialValueVector[0]	= getRandomVariableForConstant(Math.log(initialValue));
+		this.drift					= getRandomVariableForConstant(riskFreeRate - 0.5 * volatility*volatility);
+		this.volatilityOnPaths		= getRandomVariableForConstant(volatility);
 		
 		// Link model and process for delegation
 		process.setModel(this);
@@ -184,7 +184,7 @@ public class MonteCarloBlackScholesModel extends AbstractModel implements AssetM
     public RandomVariableInterface getNumeraire(double time) {
 		double numeraireValue = Math.exp(riskFreeRate * time);
 
-		return new RandomVariable(time, numeraireValue);
+        return getRandomVariableForConstant(numeraireValue);
 	}
 
 	/* (non-Javadoc)
