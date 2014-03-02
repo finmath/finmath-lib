35eed59b6ad22061243f8239b0b8c5bfd26ab15d
Christian Fries <email@christian-fries.de>
2014-03-02 22:39:17 +0000

    Added portfolio product.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@896 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/marketdata/products/Portfolio.java
@@ -0,0 +1,52 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 02.03.2014
+ */
+
+package net.finmath.marketdata.products;
+
+import java.util.ArrayList;
+
+import net.finmath.marketdata.model.AnalyticModelInterface;
+
+/**
+ * Implements the valuation of a portfolio of products implementing
+ * <code>AnalyticProductInterface</code>.
+ * 
+ * @author Christian Fries
+ */
+public class Portfolio extends AbstractAnalyticProduct implements AnalyticProductInterface {
+
+	private ArrayList<AnalyticProductInterface>	products;
+	private double[]							weights;
+
+	/**
+	 * Create a portfolio of products implementing
+	 * <code>AnalyticProductInterface</code>. The portfolio consists
+	 * of an array of products and a corresponding array of weights.
+	 * The value of the portfolio is given by the sum over
+	 * <code>
+	 * 	weights[i] * products.get(i).getValue(evaluationTime, model)
+	 * </code>
+	 * 
+	 * Note that a product in the array of products may itself be
+	 * a <code>Portfolio</code> (hence you may easily combine portfolios).
+	 * 
+	 * @param products Array of products implementing <code>AnalyticProductInterface</code>.
+	 * @param weights Array of weights used in the valuation as a multiplicator.
+	 */
+	public Portfolio(ArrayList<AnalyticProductInterface> products, double[] weights) {
+		super();
+		this.products = products;
+		this.weights = weights;
+	}
+
+	@Override
+	public double getValue(double evaluationTime, AnalyticModelInterface model) {
+		double value = 0.0;
+		for(int i=0; i<products.size(); i++) value += weights[i] * products.get(i).getValue(evaluationTime, model);
+
+		return value;
+	}
+}
