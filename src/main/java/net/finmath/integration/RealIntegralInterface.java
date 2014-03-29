6466a2d79a68742217fdb25bd39fff0cd5ecf663
Christian Fries <email@christian-fries.de>
2014-03-29 12:24:19 +0000

    Added numerical integration of real valued function w.r.t. a real (scalar) argument.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@951 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/integration/RealIntegralInterface.java
@@ -0,0 +1,24 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 23.03.2014
+ */
+
+package net.finmath.integration;
+
+import java.util.function.DoubleUnaryOperator;
+
+/**
+ * Interface for real integral. An integral is a map which
+ * maps a DoubleUnaryOperator to a double.
+ * 
+ * This is a functional interface.
+ * 
+ * @author Christian Fries
+ */
+@FunctionalInterface
+public interface RealIntegralInterface {
+
+	double integrate(DoubleUnaryOperator integrand);
+	
+}
