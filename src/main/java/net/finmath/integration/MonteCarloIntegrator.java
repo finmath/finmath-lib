a193f650c33e9c7c153baa3f372fca38ea790c7e
Christian Fries <email@christian-fries.de>
2014-03-29 12:25:03 +0000

    Added Monte-Carlo integration.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@952 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/integration/MonteCarloIntegrator.java
@@ -0,0 +1,66 @@
+package net.finmath.integration;
+
+import java.util.function.DoubleUnaryOperator;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+import cern.jet.random.engine.MersenneTwister64;
+
+
+/**
+ * A simple integrator using Monte-Carlo integration.
+ * 
+ * The constructor has an optional argument to allow
+ * parallel function evaluation. In that case, the integration rule
+ * uses Java 8 parallel streams to evaluate.
+ * 
+ * @author Christian Fries
+ */
+public class MonteCarloIntegrator extends AbstractRealIntegral{
+
+	private int		numberOfEvaluationPoints;
+	private int		seed = 3141;
+
+
+	/**
+	 * Create an integrator using Simpson's rule.
+	 * 
+	 * @param lowerBound Lower bound of the integral.
+	 * @param upperBound Upper bound of the integral.
+	 * @param numberOfEvaluationPoints Maximum number of evaluation points to be used, must be greater or equal to 3.
+	 * @param useParallelEvaluation If true, the integration rule will perform parallel evaluation of the integrand.
+	 */
+	public MonteCarloIntegrator(double lowerBound, double upperBound, int numberOfEvaluationPoints, boolean useParallelEvaluation) {
+		super(lowerBound, upperBound);
+		if(numberOfEvaluationPoints < 3) throw new IllegalArgumentException("Invalid numberOfEvaluationPoints.");
+		this.numberOfEvaluationPoints = numberOfEvaluationPoints;
+	}
+
+	/**
+	 * Create an integrator using Simpson's rule.
+	 * 
+	 * @param lowerBound Lower bound of the integral.
+	 * @param upperBound Upper bound of the integral.
+	 * @param numberOfEvaluationPoints Maximum number of evaluation points to be used.
+	 */
+	public MonteCarloIntegrator(double lowerBound, double upperBound, int numberOfEvaluationPoints) {
+		this(lowerBound, upperBound, numberOfEvaluationPoints, false);
+	}
+
+	/* (non-Javadoc)
+	 * @see net.finmath.integration.AbstractRealIntegral#integrate(java.util.function.DoubleUnaryOperator)
+	 */
+	@Override
+	public double integrate(DoubleUnaryOperator integrand) {
+		double	lowerBound			= getLowerBound();
+		double	upperBound			= getUpperBound();
+		double	range				= upperBound-lowerBound;
+		
+		// Create random number sequence generator (we use MersenneTwister64 from colt)
+		MersenneTwister64		mersenneTwister		= new MersenneTwister64(seed);
+
+		DoubleStream randomNumberSequence = IntStream.range(0, numberOfEvaluationPoints).sequential().mapToDouble(i -> mersenneTwister.nextDouble());
+
+		return randomNumberSequence.map(x -> (integrand.applyAsDouble(lowerBound + x * range))).sum() * range / numberOfEvaluationPoints;
+	}
+}
