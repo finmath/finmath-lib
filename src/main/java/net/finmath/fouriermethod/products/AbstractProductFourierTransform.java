19c120225b737833909540ff416abd3c5a979b76
Christian Fries <email@christian-fries.de>
2014-03-29 12:54:05 +0000

    Added base class for products providing characteristic functions.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@957 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/fouriermethod/products/AbstractProductFourierTransform.java
@@ -0,0 +1,73 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 23.03.2014
+ */
+
+package net.finmath.fouriermethod.products;
+
+import java.util.function.DoubleUnaryOperator;
+
+import org.apache.commons.math3.complex.Complex;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.fouriermethod.CharacteristicFunctionInterface;
+import net.finmath.fouriermethod.models.ProcessCharacteristicFunctionInterface;
+import net.finmath.integration.RealIntegralInterface;
+import net.finmath.integration.RombergRealIntegration;
+import net.finmath.integration.SimpsonRealIntegrator;
+
+/**
+ * @author Christian Fries
+ *
+ */
+public abstract class AbstractProductFourierTransform implements CharacteristicFunctionInterface {
+
+	/**
+	 * This method returns the value random variable of the product within the specified model, evaluated at a given evalutationTime.
+	 * Note: For a lattice this is often the value conditional to evalutationTime, for a Monte-Carlo simulation this is the (sum of) value discounted to evaluation time.
+	 * Cashflows prior evaluationTime are not considered.
+	 * 
+	 * @param evaluationTime The time on which this products value should be observed.
+	 * @param model The model used to price the product.
+	 * @return The random variable representing the value of the product discounted to evaluation time
+	 * @throws net.finmath.exception.CalculationException Thrown if the valuation fails, specific cause may be available via the <code>cause()</code> method.
+	 */
+	public double getValue(ProcessCharacteristicFunctionInterface model) throws CalculationException {
+
+		CharacteristicFunctionInterface modelCF = model.apply(getMaturity());
+
+		final double lineOfIntegration = 0.5 * getIntegrationDomainImagUpperBound()+getIntegrationDomainImagLowerBound();
+		DoubleUnaryOperator integrand = real -> {
+			Complex z = new Complex(real,lineOfIntegration);
+			return modelCF.apply(z.negate()).multiply(this.apply(z)).getReal();
+		};
+
+		RealIntegralInterface integrator = new SimpsonRealIntegrator(-100.0, 100.0, 20000, true);
+
+		return integrator.integrate(integrand) / 2.0 / Math.PI;
+	}
+
+	/**
+	 * Return the maturity of the associated payoff.
+	 * 
+	 * @return The maturity of the associated payoff.
+	 */
+	public abstract double getMaturity();
+	
+	/**
+	 * Return the lower bound of the imaginary part of the domain where
+	 * the characteristic function can be integrated.
+	 * 
+	 * @return the lower bound of the imaginary part of the domain of integration.
+	 */
+	public abstract double getIntegrationDomainImagLowerBound();
+	
+	/**
+	 * Return the upper bound of the imaginary part of the domain where
+	 * the characteristic function can be integrated.
+	 * 
+	 * @return the upper bound of the imaginary part of the domain of integration.
+	 */
+	public abstract double getIntegrationDomainImagUpperBound();
+}
