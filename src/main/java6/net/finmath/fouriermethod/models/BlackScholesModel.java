f265b11be391e50e4def6132220d58778d733d1b
Christian Fries <email@christian-fries.de>
2014-03-30 07:07:12 +0000

    Added Java 6 port of characteristic function for geometric Brownian motion.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@970 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java6/net/finmath/fouriermethod/models/BlackScholesModel.java
@@ -0,0 +1,51 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 23.03.2014
+ */
+
+package net.finmath.fouriermethod.models;
+
+import net.finmath.fouriermethod.CharacteristicFunctionInterface;
+
+import org.apache.commons.math3.complex.Complex;
+
+/**
+ * Implements the characteristic function of a Black Scholes model.
+ * 
+ * @author Christian Fries
+ * @author Alessandro Gnoatto
+ */
+public class BlackScholesModel implements ProcessCharacteristicFunctionInterface {
+
+	private final double initialValue;
+	private final double riskFreeRate;		// Actually the same as the drift (which is not stochastic)
+	private final double volatility;
+
+	public BlackScholesModel(double initialValue, double riskFreeRate, double volatility) {
+		super();
+		this.initialValue = initialValue;
+		this.riskFreeRate = riskFreeRate;
+		this.volatility = volatility;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.finmath.fouriermethod.models.ProcessCharacteristicFunctionInterface#apply(double)
+	 */
+	@Override
+	public CharacteristicFunctionInterface apply(final double time) {
+        return new CharacteristicFunctionInterface() {
+            @Override
+            public Complex apply(Complex argument) {
+                Complex iargument = argument.multiply(Complex.I);
+                return	iargument
+                        .multiply(
+                                iargument
+                                        .multiply(0.5*volatility*volatility*time)
+                                        .add(Math.log(initialValue)-0.5*volatility*volatility*time+riskFreeRate*time))
+                        .add(-riskFreeRate*time)
+                        .exp();
+            };
+        };
+	}
+}
