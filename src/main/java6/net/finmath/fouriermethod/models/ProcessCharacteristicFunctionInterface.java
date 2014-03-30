fc49a68dd6d07bc7212f79602387eb057f64b99d
Christian Fries <email@christian-fries.de>
2014-03-30 07:07:12 +0000

    Added Java 6 port of characteristic function for geometric Brownian motion.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@970 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java6/net/finmath/fouriermethod/models/ProcessCharacteristicFunctionInterface.java
@@ -0,0 +1,26 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 24.03.2014
+ */
+
+package net.finmath.fouriermethod.models;
+
+import net.finmath.fouriermethod.CharacteristicFunctionInterface;
+
+/**
+ * Interface which has to be implemented by models providing the
+ * characteristic functions of stochastic processes.
+ * 
+ * @author Christian Fries
+ */
+public interface ProcessCharacteristicFunctionInterface {
+
+	/**
+	 * Returns the characteristic function of X(t), where X is <code>this</code> stochastic process.
+	 * 
+	 * @param time The time at which the stochastic process is observed.
+	 * @return The characteristic function of X(t).
+	 */
+	CharacteristicFunctionInterface apply(double time);
+}
