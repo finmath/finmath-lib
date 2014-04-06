bb012f2e6a8bf3f86f23270abd7572438fa6ff29
Christian Fries <email@christian-fries.de>
2014-04-06 20:52:01 +0000

    Made CurveTest a unit test. Fixed test: fixed wrong specification of objective function (target values were provided AND used in the object function).
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1005 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/test/java/net/finmath/tests/marketdata/curves/CurveTest.java
+++ src/test/java/net/finmath/tests/marketdata/curves/CurveTest.java
@@ -9,6 +9,8 @@ package net.finmath.tests.marketdata.curves;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
+import org.junit.Test;
+
 import net.finmath.marketdata.model.curves.Curve;
 import net.finmath.marketdata.model.curves.CurveInterface;
 import net.finmath.optimizer.LevenbergMarquardt;
@@ -31,13 +33,24 @@ public class CurveTest {
 	 * @throws CloneNotSupportedException Thrown if curve cannot be cloned for optimization.
 	 */
 	public static void main(String[] args) throws SolverException, CloneNotSupportedException {
+		(new CurveTest()).testCurveFitting();
+	}
+	
+	/**
+	 * Tests fitting of curve to given data.
+	 * 
+	 * @throws SolverException Thrown if optimizer fails.
+	 * @throws CloneNotSupportedException Thrown if curve cannot be cloned for optimization.
+	 */
+	@Test
+	public void testCurveFitting() throws SolverException, CloneNotSupportedException {
 		
 		/*
 		 * Build a curve (initial guess for our fitting problem, defines the times).
 		 */
 		Curve.CurveBuilder curveBuilder = new Curve.CurveBuilder();
 		
-		curveBuilder.setInterpolationMethod(Curve.InterpolationMethod.CUBIC_SPLINE);
+		curveBuilder.setInterpolationMethod(Curve.InterpolationMethod.LINEAR);
 		curveBuilder.setExtrapolationMethod(Curve.ExtrapolationMethod.LINEAR);
 		curveBuilder.setInterpolationEntity(Curve.InterpolationEntity.VALUE);
 		
@@ -46,16 +59,18 @@ public class CurveTest {
 		curveBuilder.addPoint( 0.0 /* time */, 1.0 /* value */, false /* isParameter */);
 
 		// Add some points - which will be fitted
+		curveBuilder.addPoint( 0.5  /* time */, 2.0 /* value */, true /* isParameter */);
+		curveBuilder.addPoint( 0.75 /* time */, 2.0 /* value */, true /* isParameter */);
 		curveBuilder.addPoint( 1.0 /* time */, 2.0 /* value */, true /* isParameter */);
-		curveBuilder.addPoint( 2.0 /* time */, 2.0 /* value */, true /* isParameter */);
+		curveBuilder.addPoint( 2.2 /* time */, 2.0 /* value */, true /* isParameter */);
 		curveBuilder.addPoint( 3.0 /* time */, 2.0 /* value */, true /* isParameter */);
 
 		final Curve curve = curveBuilder.build();
 		
 		/*
-		 * Create data to which the curve should be fitted
+		 * Create data to which the curve should be fitted to
 		 */
-		final double[] times		= { 0.0,  0.5, 0.75, 1.0, 1.5, 1.75, 2.3 };
+		final double[] givenTimes	= { 0.0,  0.5, 0.75, 1.0, 1.5, 1.75, 2.5 };
 		final double[] givenValues	= { 3.5, 12.3, 13.2, 7.5, 5.5, 2.9,  4.4 };
 
 		/*
@@ -73,15 +88,15 @@ public class CurveTest {
 			@Override
 			public void setValues(double[] parameters, double[] values) throws SolverException {
 				
-				CurveInterface cuveGuess = null;
+				CurveInterface curveGuess = null;
 				try {
-					cuveGuess = curve.getCloneForParameter(parameters);
+					curveGuess = curve.getCloneForParameter(parameters);
 				} catch (CloneNotSupportedException e) {
 					throw new SolverException(e);
 				}
 
 				for(int valueIndex=0; valueIndex<values.length; valueIndex++) {
-					values[valueIndex] = cuveGuess.getValue(times[valueIndex]) - givenValues[valueIndex];
+					values[valueIndex] = curveGuess.getValue(givenTimes[valueIndex]);
 				}
 			}
 		};
@@ -95,5 +110,18 @@ public class CurveTest {
 		for(double time = -2.0; time < 5.0; time += 0.1) {
 			System.out.println(numberFormat.format(time) + "\t" + numberFormat.format(fittedCurve.getValue(time)));
 		}
+
+		// Check fitted curve
+		double errorSum = 0.0;
+		for(int pointIndex = 0; pointIndex<givenTimes.length; pointIndex++) {
+			errorSum += fittedCurve.getValue(givenTimes[pointIndex]) - givenValues[pointIndex];
+		}
+		System.out.println("Mean deviation: " + errorSum);
+		
+		/*
+		 * Test: With the given data, the fit cannot over come that at 0.0 we have an error of -2.5.
+		 * Hence we test if the mean deviation is -2.5 (the optimizer reduces the variance)
+		 */
+		org.junit.Assert.assertTrue(Math.abs(errorSum - -2.5) < 1E-5);
 	}
 }
