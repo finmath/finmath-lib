8fbb6b32aa5a2a75027d0f015a68195f4bd26b99
Christian Fries <email@christian-fries.de>
2014-05-24 10:38:20 +0000

    Added PiecewiseCurve and ForwardCurveWithFixings (a PiecewiseCurve for Forwards) to define piecewise curves. No interpolation between the pieces is performed.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1071 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/marketdata/model/curves/ForwardCurveWithFixings.java
@@ -0,0 +1,82 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 22.05.2014
+ */
+
+package net.finmath.marketdata.model.curves;
+
+import net.finmath.marketdata.model.AnalyticModelInterface;
+
+/**
+ * @author Christian Fries
+ *
+ */
+public class ForwardCurveWithFixings extends PiecewiseCurve implements ForwardCurveInterface {
+
+	/**
+	 * @param curveInterface
+	 * @param fixedPartCurve
+	 * @param fixedPartStartTime
+	 * @param fixedPartEndTime
+	 */
+	public ForwardCurveWithFixings(ForwardCurveInterface curveInterface, ForwardCurveInterface fixedPartCurve, double fixedPartStartTime, double fixedPartEndTime) {
+		super(curveInterface, fixedPartCurve, fixedPartStartTime, fixedPartEndTime);
+	}
+
+	@Override
+	public double getForward(AnalyticModelInterface model, double fixingTime) {
+		if(fixingTime > this.getFixedPartStartTime() && fixingTime < this.getFixedPartEndTime()) {
+			return ((ForwardCurveInterface)getFixedPartCurve()).getForward(model, fixingTime);
+		}
+		else {
+			return ((ForwardCurveInterface)getBaseCurve()).getForward(model, fixingTime);
+		}
+	}
+
+	@Override
+	public double getForward(AnalyticModelInterface model, double fixingTime, double paymentOffset) {
+		if(fixingTime > this.getFixedPartStartTime() && fixingTime < this.getFixedPartEndTime()) {
+			return ((ForwardCurveInterface)getFixedPartCurve()).getForward(model, fixingTime, paymentOffset);
+		}
+		else {
+			return ((ForwardCurveInterface)getBaseCurve()).getForward(model, fixingTime, paymentOffset);
+		}
+	}
+
+	/**
+	 * Returns the forwards for a given vector fixing times.
+	 * 
+	 * @param model An analytic model providing a context. The discount curve (if needed) is obtained from this model.
+	 * @param fixingTimes The given fixing times.
+	 * @return The forward rates.
+	 */
+	public double[] getForwards(AnalyticModelInterface model, double[] fixingTimes)
+	{
+		double[] values = new double[fixingTimes.length];
+
+		for(int i=0; i<fixingTimes.length; i++) values[i] = getForward(model, fixingTimes[i]);
+
+		return values;
+	}
+
+	@Override
+	public String getDiscountCurveName() {
+		return ((ForwardCurveInterface)getBaseCurve()).getDiscountCurveName();
+	}
+
+	@Override
+	public double getPaymentOffset(double fixingTime) {
+		return ((ForwardCurveInterface)getBaseCurve()).getPaymentOffset(fixingTime);
+	}
+
+	@Override
+	public CurveInterface getCloneForParameter(double[] value) throws CloneNotSupportedException {
+		return new ForwardCurveWithFixings((ForwardCurveInterface)getBaseCurve().getCloneForParameter(value), (ForwardCurveInterface)getFixedPartCurve(), getFixedPartStartTime(), getFixedPartEndTime());
+	}
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return new ForwardCurveWithFixings((ForwardCurveInterface)getBaseCurve().clone(), (ForwardCurveInterface)getFixedPartCurve(), getFixedPartStartTime(), getFixedPartEndTime());
+	}
+}
