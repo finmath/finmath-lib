616d769541823e59de503b8ac8f7a138f6829489
Christian Fries <email@christian-fries.de>
2014-07-26 23:31:48 +0000

    Added small utility class to use Future<V> in a non-concurrent context.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1172 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/concurrency/FutureWrapper.java
@@ -0,0 +1,52 @@
+package net.finmath.concurrency;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Implementation of the Future<V> interface,
+ * without any concurrent execution.
+ * 
+ * This wrapper comes handy, if we want lazy, late valuation
+ * to replace concurrent valuation.
+ * 
+ * @author Christian Fries
+ *
+ * @param <V>
+ */
+public class FutureWrapper<V> implements Future<V> {
+
+	private V object;
+	
+	public FutureWrapper(V object) {
+		super();
+		this.object = object;
+	}
+
+	@Override
+	public boolean cancel(boolean mayInterruptIfRunning) {
+		return false;
+	}
+
+	@Override
+	public V get() throws InterruptedException, ExecutionException {
+		return object;
+	}
+
+	@Override
+	public V get(long arg0, TimeUnit arg1) throws InterruptedException, ExecutionException, TimeoutException {
+		return object;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return false;
+	}
+
+	@Override
+	public boolean isDone() {
+		return true;
+	}
+}
