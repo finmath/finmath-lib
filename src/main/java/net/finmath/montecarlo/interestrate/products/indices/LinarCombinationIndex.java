f27186dd2b0e2a84ad6f23905a137d2c6806b262
Christian Fries <email@christian-fries.de>
2014-07-27 00:02:25 +0000

    Added a linear combination index.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1173 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/montecarlo/interestrate/products/indices/LinarCombinationIndex.java
@@ -0,0 +1,48 @@
+/*
+ * Created on 06.12.2009
+ *
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ */
+package net.finmath.montecarlo.interestrate.products.indices;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+import net.finmath.montecarlo.interestrate.products.components.AbstractProductComponent;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * A linear combination index paying scaling1 * index1(t) + scaling2 * index2(t)
+ * 
+ * @author Christian Fries
+ */
+public class LinarCombinationIndex extends AbstractIndex {
+
+	private static final long serialVersionUID = -8181742829635380940L;
+
+	private AbstractProductComponent index1;
+	private AbstractProductComponent index2;
+	private double scaling1;
+	private double scaling2;
+
+    /**
+     * Create a linear combination index paying scaling1 * index1(t) + scaling2 * index2(t)
+     * 
+     * @param scaling1 Scaling for first index.
+     * @param index1 First index.
+     * @param scaling2 Scaling for second index.
+     * @param index2 Second index.
+     */
+    public LinarCombinationIndex(double scaling1, AbstractProductComponent index1, double scaling2, AbstractProductComponent index2) {
+		super();
+		this.scaling1	= scaling1;
+		this.index1		= index1;
+		this.scaling2	= scaling2;
+		this.index2		= index2;
+	}
+
+    @Override
+    public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {
+    	return index1.getValue(evaluationTime, model).mult(scaling1)
+    			.add(index2.getValue(evaluationTime, model).mult(scaling2));
+    }
+}
