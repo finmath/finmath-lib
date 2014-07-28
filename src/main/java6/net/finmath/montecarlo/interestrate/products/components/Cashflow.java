de7c229f0c69b0f87f5f5d46d706071e0a8edee1
Christian Fries <email@christian-fries.de>
2014-07-28 21:23:23 +0000

    Added simple cash flow component.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1193 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java6/net/finmath/montecarlo/interestrate/products/components/Cashflow.java
@@ -0,0 +1,70 @@
+/*
+ * Created on 22.11.2009
+ */
+package net.finmath.montecarlo.interestrate.products.components;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.montecarlo.RandomVariable;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * A single deterministic cashflow at a fixed time
+ * 
+ * @author Christian Fries
+ * @version 1.1
+ */
+public class Cashflow extends AbstractProductComponent {
+
+	private static final long serialVersionUID = 2336470863786839896L;
+
+	private final double flowAmount;
+	private final double flowDate;
+	private final boolean isPayer;
+
+
+
+	/**
+	 * Create a single deterministic cashflow at a fixed time.
+	 * 
+	 * @param flowAmount The amount of the cash flow.
+	 * @param flowDate The flow date.
+	 * @param isPayer If true, this cash flow will be multiplied by -1 prior valuation.
+	 */
+	public Cashflow(double flowAmount, double flowDate, boolean isPayer) {
+		super();
+		this.flowAmount = flowAmount;
+		this.flowDate = flowDate;
+		this.isPayer = isPayer;
+	}
+
+
+	/**
+	 * This method returns the value random variable of the product within the specified model, evaluated at a given evalutationTime.
+	 * Note: For a lattice this is often the value conditional to evalutationTime, for a Monte-Carlo simulation this is the (sum of) value discounted to evaluation time.
+	 * Cashflows prior evaluationTime are not considered.
+	 * 
+	 * @param evaluationTime The time on which this products value should be observed.
+	 * @param model The model used to price the product.
+	 * @return The random variable representing the value of the product discounted to evaluation time
+	 * @throws net.finmath.exception.CalculationException Thrown if the valuation fails, specific cause may be available via the <code>cause()</code> method. 
+	 */
+	@Override
+	public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {        
+
+		if(evaluationTime >= flowDate) return new RandomVariable(0.0);
+
+		// Get random variables
+		RandomVariableInterface	numeraire				= model.getNumeraire(flowDate);
+		RandomVariableInterface	numeraireAtEval			= model.getNumeraire(evaluationTime);
+		//        RandomVariableInterface	monteCarloProbabilities	= model.getMonteCarloWeights(getPaymentDate());
+
+		RandomVariableInterface values = new RandomVariable(flowAmount);
+		if(isPayer) values = values.mult(-1.0);
+
+		values = values.div(numeraire).mult(numeraireAtEval);
+
+		// Return values
+		return values;	
+	}    
+}
