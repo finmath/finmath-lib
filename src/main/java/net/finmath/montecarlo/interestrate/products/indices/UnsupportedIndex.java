eb2d90e1cb7347b21e6320604c1132378ffdb90e
Christian Fries <email@christian-fries.de>
2014-08-15 18:34:00 +0000

    Added an unsupported index :-) - see JavaDoc for details.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1215 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/montecarlo/interestrate/products/indices/UnsupportedIndex.java
@@ -0,0 +1,42 @@
+/*
+ * Created on 06.12.2009
+ *
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ */
+package net.finmath.montecarlo.interestrate.products.indices;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * An index throwing an exception if his <code>getValue</code> method is called.
+ * 
+ * This class can be used to created indices which triggers an exception only upon usage (i.e., late).
+ * 
+ * A typical use case is a financial product referencing an index in one of its past payments,
+ * which is not supported by the current model. In that case the product can be valued as of a future
+ * date, but no necessarily as of a past date.
+ * 
+ * @author Christian Fries
+ */
+public class UnsupportedIndex extends AbstractIndex {
+
+	private static final long serialVersionUID = 5375406324063846793L;
+	private final Exception exception;
+	
+	/**
+	 * Creates an unsupported index throwing an exception if his <code>getValue</code> method is called.
+	 * 
+     * @param exception The exception.
+     */
+    public UnsupportedIndex(Exception exception) {
+	    super();
+	    this.exception = exception;
+    }
+
+    @Override
+    public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {
+        throw new CalculationException(exception);
+    }
+}
