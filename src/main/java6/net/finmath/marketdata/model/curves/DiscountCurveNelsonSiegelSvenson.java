04e97c47c8ed14743842a8af16359e2eeceef2a4
Christian Fries <email@christian-fries.de>
2014-08-15 07:05:52 +0000

    Created NSS discount curve and forward curve.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1211 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java6/net/finmath/marketdata/model/curves/DiscountCurveNelsonSiegelSvenson.java
@@ -0,0 +1,98 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ *
+ * Created on 20.05.2005
+ */
+package net.finmath.marketdata.model.curves;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import net.finmath.marketdata.model.AnalyticModelInterface;
+
+/**
+ * Implementation of a discount factor curve given by a Nelson-Siegel-Svensson (NSS) parametrization.
+ * In the NSS parametrization the zero rate \( r(T) \) is given by
+ * 
+ * \( r(T) = \beta_0 + \beta_1 \frac{(1-x_0)/(T/\tau_0)} + \beta_2 ( \frac{(1-x_0)/(T/\tau_0)} - x_0) + \beta_3 ( \frac{(1-x_2)/(T/\tau_1)} - x_1) \)
+ * 
+ * where \( x_0 = \exp(-T/\tau_0) \) and \( x_1 = \exp(-T/\tau_1) \).
+ * 
+ * The sub-family of curve with \( \beta_3 = 0 \) is called Nelson-Siegel parametrization.
+ * 
+ * @author Christian Fries
+ */
+public class DiscountCurveNelsonSiegelSvenson extends AbstractCurve implements Serializable, DiscountCurveInterface {
+
+	private static final long serialVersionUID = 8024640795839972709L;
+
+	private final double[] parameter;
+
+	public DiscountCurveNelsonSiegelSvenson(String name, Calendar referenceDate, double[] parameter) {
+		super(name, referenceDate);
+		
+		this.parameter = parameter; 
+	}
+
+	@Override
+	public double getDiscountFactor(double maturity)
+	{
+		return getDiscountFactor(null, maturity);
+	}
+
+	/* (non-Javadoc)
+	 * @see net.finmath.marketdata.model.curves.DiscountCurveInterface#getDiscountFactor(double)
+	 */
+	@Override
+	public double getDiscountFactor(AnalyticModelInterface model, double maturity)
+	{
+		double beta1 = parameter[0];
+		double beta2 = parameter[1];
+		double beta3 = parameter[2];
+		double beta4 = parameter[3];
+		double tau1	= parameter[4];
+		double tau2 = parameter[5];
+
+		double x1 = tau1 > 0 ? Math.exp(-maturity/tau1) : 0.0;
+		double x2 = tau2 > 0 ? Math.exp(-maturity/tau2) : 0.0;
+
+		double y1 = (maturity > 0.0 ? (1.0-x1)/maturity*tau1 : 0.0);
+		double y2 = (maturity > 0.0 ? (1.0-x2)/maturity*tau2 : 0.0);
+		
+		double zeroRate = beta1 + beta2 * y1 + beta3 * (y1-x1) + beta4 * (y2-x2);
+
+		return Math.exp(- zeroRate * maturity);
+	}
+
+	@Override
+	public double getValue(AnalyticModelInterface model, double time) {
+		return getDiscountFactor(model, time);
+	}
+
+	@Override
+	public CurveBuilderInterface getCloneBuilder() throws CloneNotSupportedException {
+		return null;
+	}
+
+	@Override
+	public double[] getParameter() {
+		return parameter;
+	}
+
+	@Override
+	@Deprecated
+	public void setParameter(double[] parameter) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public DiscountCurveNelsonSiegelSvenson clone() throws CloneNotSupportedException {
+		return (DiscountCurveNelsonSiegelSvenson)super.clone();
+	}
+
+	@Override
+	public DiscountCurveNelsonSiegelSvenson getCloneForParameter(double[] value) throws CloneNotSupportedException {
+		return new DiscountCurveNelsonSiegelSvenson(getName(), getReferenceDate(), value);
+	}
+}
---
+++ src/main/java6/net/finmath/marketdata/model/curves/ForwardCurveNelsonSiegelSvenson.java
@@ -0,0 +1,130 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ *
+ * Created on 20.05.2005
+ */
+package net.finmath.marketdata.model.curves;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import net.finmath.marketdata.model.AnalyticModel;
+import net.finmath.marketdata.model.AnalyticModelInterface;
+import net.finmath.time.ScheduleGenerator.DaycountConvention;
+import net.finmath.time.businessdaycalendar.BusinessdayCalendarInterface;
+import net.finmath.time.daycount.DayCountConventionInterface;
+import net.finmath.time.daycount.DayCountConvention_ACT_360;
+import net.finmath.time.daycount.DayCountConvention_ACT_365;
+
+/**
+ * Implementation of a forward given by a Nelson-Siegel-Svensson (NSS) parametrization.
+ * In the NSS parametrization the zero rate \( r(T) \) is given by
+ * 
+ * \( r(T) = \beta_0 + \beta_1 \frac{(1-x_0)/(T/\tau_0)} + \beta_2 ( \frac{(1-x_0)/(T/\tau_0)} - x_0) + \beta_3 ( \frac{(1-x_2)/(T/\tau_1)} - x_1) \)
+ * 
+ * where \( x_0 = \exp(-T/\tau_0) \) and \( x_1 = \exp(-T/\tau_1) \).
+ * 
+ * The sub-family of curve with \( \beta_3 = 0 \) is called Nelson-Siegel parametrization.
+ * 
+ * @author Christian Fries
+ */
+public class ForwardCurveNelsonSiegelSvenson extends AbstractCurve implements Serializable, ForwardCurveInterface {
+
+	private static final long serialVersionUID = 8024640795839972709L;
+
+	private String paymentOffsetCode;
+	private BusinessdayCalendarInterface paymentBusinessdayCalendar;
+	private BusinessdayCalendarInterface.DateRollConvention paymentDateRollConvention;
+	private DayCountConventionInterface daycountConvention;
+	
+	private DiscountCurveNelsonSiegelSvenson discountCurve;
+	
+	/**
+	 * @param name The name of the curve. The curve can be fetched under this name when being part of an {@link AnalyticModel}.
+	 * @param referenceDate The reference date to the curve, i.e., the date associated with t=0.
+	 * @param paymentOffsetCode The payment offset code, like 3M, 6M, 12M, etc., used incalculating forwards from disocunt factors.
+	 * @param paymentBusinessdayCalendar The payment businessday calendar.
+	 * @param paymentDateRollConvention The payment date roll convention.
+	 * @param daycountConvention The daycount convention.
+	 * @param parameter The six Nelson Siegel Svenson parameters, beta_0, ..., beta_3, tau_0, tau_1.
+	 */
+	public ForwardCurveNelsonSiegelSvenson(String name, Calendar referenceDate, String paymentOffsetCode, BusinessdayCalendarInterface paymentBusinessdayCalendar, BusinessdayCalendarInterface.DateRollConvention paymentDateRollConvention, DayCountConventionInterface daycountConvention, double[] parameter) {
+		super(name, referenceDate);
+		this.paymentOffsetCode = paymentOffsetCode;
+		this.paymentBusinessdayCalendar = paymentBusinessdayCalendar;
+		this.paymentDateRollConvention = paymentDateRollConvention;
+		this.daycountConvention = daycountConvention;
+		
+		discountCurve = new DiscountCurveNelsonSiegelSvenson(name, referenceDate, parameter);
+	}
+
+	@Override
+	public double getForward(AnalyticModelInterface model, double fixingTime) {
+		return getForward(model, fixingTime, getPaymentOffset(fixingTime));
+	}
+
+	@Override
+	public double getForward(AnalyticModelInterface model, double fixingTime, double paymentOffset) {
+		double daycountFraction = getCurveTime(fixingTime + paymentOffset)-getCurveTime(fixingTime);
+		return (discountCurve.getDiscountFactor(model, getCurveTime(fixingTime)) / discountCurve.getDiscountFactor(model, getCurveTime(fixingTime + paymentOffset)) - 1.0) / daycountFraction;
+	}
+
+	public double getCurveTime(double time) {
+		// Create model date from model time
+			Calendar modelDate	= (Calendar)getReferenceDate().clone();
+			modelDate.add(Calendar.DAY_OF_YEAR, (int)(time*365));
+
+			double paymentTime = daycountConvention.getDaycountFraction(getReferenceDate(), modelDate);
+			return paymentTime;
+	}
+
+	@Override
+	public String getDiscountCurveName() {
+		return null;
+	}
+
+	@Override
+	public CurveBuilderInterface getCloneBuilder() throws CloneNotSupportedException {
+		return null;
+	}
+
+	@Override
+	public ForwardCurveNelsonSiegelSvenson clone() throws CloneNotSupportedException {
+		return (ForwardCurveNelsonSiegelSvenson)super.clone();
+	}
+
+	@Override
+	public ForwardCurveNelsonSiegelSvenson getCloneForParameter(double[] value) throws CloneNotSupportedException {
+		throw new CloneNotSupportedException();
+	}
+
+	@Override
+	public double getValue(AnalyticModelInterface model, double time) {
+		return getForward(model, time, getPaymentOffset(time));
+	}
+
+	@Override
+	public double[] getParameter() {
+		return discountCurve.getParameter();
+	}
+
+	/* (non-Javadoc)
+	 * @see net.finmath.marketdata.calibration.ParameterObjectInterface#setParameter(double[])
+	 */
+	@Override
+	public void setParameter(double[] parameter) {
+		discountCurve.setParameter(parameter);
+	}
+
+	/*
+	 * @TODO: This operates on model internal time and not on curve time - which is inefficient. Performance improvement possible.
+	 */
+	@Override
+	public double getPaymentOffset(double fixingTime) {
+				Calendar paymentDate	= (Calendar)getReferenceDate().clone();
+				paymentDate.add(Calendar.DAY_OF_YEAR, (int)(fixingTime*365));
+				paymentDate = paymentBusinessdayCalendar.getAdjustedDate(paymentDate, paymentOffsetCode, paymentDateRollConvention);
+				double paymentTime = (new DayCountConvention_ACT_365()).getDaycountFraction(getReferenceDate(), paymentDate);
+				return paymentTime-fixingTime;
+	}
+}
