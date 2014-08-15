57d7df1d32e2ab93c4d1b1befe5e8379593744c0
Christian Fries <email@christian-fries.de>
2014-08-15 07:05:52 +0000

    Created NSS discount curve and forward curve.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1211 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/marketdata/model/curves/DiscountCurveFromProductOfCurves.java
@@ -0,0 +1,77 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ *
+ * Created on 20.05.2005
+ */
+package net.finmath.marketdata.model.curves;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import net.finmath.marketdata.model.AnalyticModelInterface;
+
+/**
+ * A discount curve derived from other discount curves
+ * by multiplying the discount factors.
+ * 
+ * @author Christian Fries
+ */
+public class DiscountCurveFromProductOfCurves extends AbstractCurve implements Serializable, DiscountCurveInterface {
+	
+	private static final long serialVersionUID = 8850409340966149755L;
+
+	private DiscountCurveInterface[] curves;
+
+	/**
+     * Create a discount curve using one or more curves.
+     * 
+	 * @param name The name of this curve.
+	 * @param referenceDate The reference date of this curve.
+     * @param curves Argument list or array of curves.
+     */
+    public DiscountCurveFromProductOfCurves(String name, Calendar referenceDate, DiscountCurveInterface... curves) {
+    	super(name, referenceDate);
+
+    	this.curves = curves;
+    }
+
+	@Override
+	public double getDiscountFactor(double maturity) {
+		return getDiscountFactor(null, maturity);
+	}
+
+	@Override
+	public double getDiscountFactor(AnalyticModelInterface model, double maturity) {
+		double discountFactor = 1.0;
+
+		for(DiscountCurveInterface curve : curves) discountFactor *= curve.getDiscountFactor(model, maturity);
+		
+		return discountFactor;
+	}
+
+	@Override
+	public double getValue(AnalyticModelInterface model, double time) {
+		return getDiscountFactor(model, time);
+	}
+
+
+	@Override
+	public double[] getParameter() {
+		return null;
+	}
+
+	@Override
+	public void setParameter(double[] parameter) {
+	}
+
+
+	/* (non-Javadoc)
+	 * @see net.finmath.marketdata.model.curves.CurveInterface#getCloneBuilder()
+	 */
+	@Override
+	public CurveBuilderInterface getCloneBuilder()
+			throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
