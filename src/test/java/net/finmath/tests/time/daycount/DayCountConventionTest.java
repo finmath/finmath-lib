4f138d709334f9af37b5396b289c427f853dfe17
Christian Fries <email@christian-fries.de>
2014-08-30 11:28:15 +0000

    Added test case for act/act (ICMA) convention.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1247 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/test/java/net/finmath/tests/time/daycount/DayCountConventionTest.java
+++ src/test/java/net/finmath/tests/time/daycount/DayCountConventionTest.java
@@ -5,11 +5,14 @@
  */
 package net.finmath.tests.time.daycount;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import net.finmath.time.Period;
 import net.finmath.time.daycount.DayCountConventionInterface;
 import net.finmath.time.daycount.DayCountConvention_30E_360;
+import net.finmath.time.daycount.DayCountConvention_ACT_ACT_ICMA;
 import net.finmath.time.daycount.DayCountConvention_ACT_ACT_ISDA;
 
 import org.junit.Assert;
@@ -85,5 +88,82 @@ public class DayCountConventionTest {
 		//
 		daycountFraction = daycountConvention.getDaycountFraction(new GregorianCalendar(1999,Calendar.FEBRUARY,1), new GregorianCalendar(1999,Calendar.JULY,1));
 		Assert.assertTrue(Math.abs(daycountFraction - 150.0/360.0) < 1.0E-8);
+	}
+
+	@Test
+	public void testDayCountConventionAdditivity_ACT_ACT_ICMA() {
+		ArrayList<Period> periods = new ArrayList<Period>();
+		Calendar start, end;
+		for(int i=1980; i<2100; i++) {
+			start	= new GregorianCalendar(i, Calendar.FEBRUARY   , 15);
+			end		= new GregorianCalendar(i, Calendar.FEBRUARY +3, 31);
+			periods.add(new Period(start, end, start, end));
+
+			start	= new GregorianCalendar(i, Calendar.FEBRUARY +3  , 15);
+			end		= new GregorianCalendar(i, Calendar.FEBRUARY +6, 31);
+			periods.add(new Period(start, end, start, end));
+
+			start	= new GregorianCalendar(i, Calendar.FEBRUARY +6  , 15);
+			end		= new GregorianCalendar(i, Calendar.FEBRUARY +9, 31);
+			periods.add(new Period(start, end, start, end));
+
+			start	= new GregorianCalendar(i, Calendar.FEBRUARY +9  , 15);
+			end		= new GregorianCalendar(i+1, Calendar.FEBRUARY +0, 31);
+			periods.add(new Period(start, end, start, end));
+		}
+		DayCountConventionInterface daycountConvention = new DayCountConvention_ACT_ACT_ICMA(periods, 4);
+
+		// Test additivity of day count fraction
+		double daycountFractionTotal = daycountConvention.getDaycountFraction(new GregorianCalendar(2012,Calendar.MARCH,19), new GregorianCalendar(2013,Calendar.AUGUST,4));
+		double daycountFractionPart1 = daycountConvention.getDaycountFraction(new GregorianCalendar(2012,Calendar.MARCH,19), new GregorianCalendar(2013,Calendar.JANUARY,1));
+		double daycountFractionPart2 = daycountConvention.getDaycountFraction(new GregorianCalendar(2013,Calendar.JANUARY,1), new GregorianCalendar(2013,Calendar.AUGUST,4));
+		Assert.assertTrue(Math.abs(daycountFractionTotal - (daycountFractionPart1 + daycountFractionPart2)) < 1.0E-8);
+	}
+
+	@Test
+	public void testDayCountConventionConsistency_ACT_ACT_ICMA_versus_ACT_ACT_ISDA() {
+		/*
+		 * Quarterly periods for ICMA
+		 */
+		ArrayList<Period> periods = new ArrayList<Period>();
+		for(int i=1980; i<2100; i++) {
+			Calendar start	= new GregorianCalendar(i, Calendar.JANUARY, 1);
+			Calendar end	= new GregorianCalendar(i, Calendar.DECEMBER, 31);
+			periods.add(new Period(start, end, start, end));
+		}
+		DayCountConventionInterface daycountConventionICMA = new DayCountConvention_ACT_ACT_ICMA(periods, 1);
+
+		DayCountConventionInterface daycountConventionISDA = new DayCountConvention_ACT_ACT_ISDA();
+
+		Calendar[] startDates = new GregorianCalendar[] {
+				new GregorianCalendar(2003,Calendar.NOVEMBER,1),
+				new GregorianCalendar(1999,Calendar.FEBRUARY,1),
+				new GregorianCalendar(1999,Calendar.JULY,1),
+				new GregorianCalendar(2002,Calendar.AUGUST,15),
+				new GregorianCalendar(2003,Calendar.JULY,15),
+				new GregorianCalendar(2000,Calendar.JANUARY,30),
+				new GregorianCalendar(1999,Calendar.NOVEMBER,30),
+				new GregorianCalendar(1999,Calendar.JANUARY,1),
+				new GregorianCalendar(2014,Calendar.FEBRUARY,1)
+		};
+
+		Calendar[] endDates = new GregorianCalendar[] {
+				new GregorianCalendar(2004,Calendar.MAY,1),
+				new GregorianCalendar(1999,Calendar.JULY,1),
+				new GregorianCalendar(2000,Calendar.JULY,1),
+				new GregorianCalendar(2003,Calendar.JULY,15),
+				new GregorianCalendar(2004,Calendar.JANUARY,15),
+				new GregorianCalendar(2000,Calendar.JUNE,30),
+				new GregorianCalendar(2000,Calendar.APRIL,30),
+				new GregorianCalendar(2014,Calendar.MARCH,1),
+				new GregorianCalendar(2014,Calendar.MARCH,1)
+		};
+
+		for(int i=0; i<startDates.length; i++) {
+			double daycountFractionICMA = daycountConventionICMA.getDaycount(startDates[i], endDates[i]);
+			double daycountFractionISDA = daycountConventionISDA.getDaycount(startDates[i], endDates[i]);
+			
+			Assert.assertTrue(Math.abs(daycountFractionICMA - daycountFractionISDA) < 1.0E-8);
+		}
 	}
 }
