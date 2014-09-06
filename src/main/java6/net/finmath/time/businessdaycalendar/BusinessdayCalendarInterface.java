12f71dbe2e1690dc749f8e82c370c5edefc6eb25
Christian Fries <email@christian-fries.de>
2014-09-06 09:19:13 +0000

    Changed BusinessdayCalendar to return a clone when calling for a date adjustment - always, even when no adjustment was done.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1255 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/main/java6/net/finmath/time/businessdaycalendar/BusinessdayCalendarInterface.java
+++ src/main/java6/net/finmath/time/businessdaycalendar/BusinessdayCalendarInterface.java
@@ -50,6 +50,9 @@ public interface BusinessdayCalendarInterface {
 	/**
 	 * Get an adjusted date for a given date.
 	 * 
+	 * Since <code>java.util.Calendar</code> is not immutable this method has to return
+	 * a clone of date, even if the roll convention is not resulting in an adjustment.
+	 * 
 	 * @param date Given date to be adjusted.
 	 * @param dateRollConvention The date roll convention to be used for the adjustment.
 	 * @return The adjusted date applying dateRollConvention to the given date.
@@ -58,6 +61,9 @@ public interface BusinessdayCalendarInterface {
 
 	/**
 	 * Get an adjusted date for a given date and offset code.
+	 * 
+	 * Since <code>java.util.Calendar</code> is not immutable this method has to return
+	 * a clone of baseDate, even if the roll convention is not resulting in an adjustment.
 	 * 
 	 * First we create a new date by "adding" an offset to the base date.
 	 * The offset may be given by codes like 1D, 2D, 1W, 2W, 1M, 2M, 3M,
@@ -75,6 +81,9 @@ public interface BusinessdayCalendarInterface {
 
 	/**
 	 * Find a new date by adding the given number of business days to a given base date.
+	 * 
+	 * Since <code>java.util.Calendar</code> is not immutable this method has to return
+	 * a clone of baseDate, even if the roll convention is not resulting in an adjustment.
 	 * 
 	 * If baseDate is not a business date and businessDays is zero, the method returns the next business day.
 	 *  
