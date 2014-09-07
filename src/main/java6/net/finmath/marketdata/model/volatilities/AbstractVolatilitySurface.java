21ac1047e8bed17c962e7060738c3a13e7979fcc
Christian Fries <email@christian-fries.de>
2014-09-07 19:09:07 +0000

    Pulling up the conversion of quoting conventions to abstract base class.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1258 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/main/java6/net/finmath/marketdata/model/volatilities/AbstractVolatilitySurface.java
+++ src/main/java6/net/finmath/marketdata/model/volatilities/AbstractVolatilitySurface.java
@@ -7,6 +7,11 @@ package net.finmath.marketdata.model.volatilities;
 
 import java.util.Calendar;
 
+import net.finmath.functions.AnalyticFormulas;
+import net.finmath.marketdata.model.AnalyticModelInterface;
+import net.finmath.marketdata.model.curves.DiscountCurveInterface;
+import net.finmath.marketdata.model.curves.ForwardCurveInterface;
+
 /**
  * Abstract base class for a volatility surface. It stores the name of the surface and
  * provides some convenient way of getting values.
@@ -17,6 +22,9 @@ public abstract class AbstractVolatilitySurface implements VolatilitySurfaceInte
 
 	private	final	Calendar	referenceDate;
 	private final	String		name;
+	protected ForwardCurveInterface forwardCurve;
+	protected DiscountCurveInterface discountCurve;
+	protected QuotingConvention quotingConvention;
 
 	public AbstractVolatilitySurface(String name, Calendar referenceDate) {
 		super();
@@ -47,5 +55,58 @@ public abstract class AbstractVolatilitySurface implements VolatilitySurfaceInte
 	@Override
 	public VolatilitySurfaceInterface getCloneForParameter(double[] value) throws CloneNotSupportedException {
 		throw new CloneNotSupportedException();
+	}
+
+	@Override
+	public QuotingConvention getQuotingConvention() {
+		return quotingConvention;
+	}
+
+	/**
+	 * Convert the value of a caplet from on quoting convention to another quoting convention.
+	 * 
+	 * @param model An analytic model providing the context when fetching required market date.
+	 * @param optionMaturity Option maturity of the caplet.
+	 * @param optionStrike Option strike of the cpalet.
+	 * @param value Value of the caplet given in the form of <code>fromQuotingConvention</code>.
+	 * @param fromQuotingConvention The quoting convention of the given value.
+	 * @param toQuotingConvention The quoting convention requested.
+	 * @return Value of the caplet given in the form of <code>toQuotingConvention</code>. 
+	 */
+	public double convertFromTo(AnalyticModelInterface model, double optionMaturity, double optionStrike, double value, QuotingConvention fromQuotingConvention, QuotingConvention toQuotingConvention) {
+
+		if(fromQuotingConvention.equals(toQuotingConvention)) return value;
+
+		double forward = forwardCurve.getForward(null, optionMaturity);
+		double payoffUnit = discountCurve.getDiscountFactor(optionMaturity+forwardCurve.getPaymentOffset(optionMaturity)) * forwardCurve.getPaymentOffset(optionMaturity);
+
+		if(toQuotingConvention.equals(QuotingConvention.PRICE) && fromQuotingConvention.equals(QuotingConvention.VOLATILITYLOGNORMAL)) {
+			return AnalyticFormulas.blackScholesGeneralizedOptionValue(forward, value, optionMaturity, optionStrike, payoffUnit);
+		}
+		else if(toQuotingConvention.equals(QuotingConvention.PRICE) && fromQuotingConvention.equals(QuotingConvention.VOLATILITYNORMAL)) {
+			return AnalyticFormulas.bachelierOptionValue(forward, value, optionMaturity, optionStrike, payoffUnit);
+		}
+		else if(toQuotingConvention.equals(QuotingConvention.VOLATILITYLOGNORMAL) && fromQuotingConvention.equals(QuotingConvention.PRICE)) {
+			return AnalyticFormulas.blackScholesOptionImpliedVolatility(forward, optionMaturity, optionStrike, payoffUnit, value);
+		}
+		else if(toQuotingConvention.equals(QuotingConvention.VOLATILITYNORMAL) && fromQuotingConvention.equals(QuotingConvention.PRICE)) {
+			return AnalyticFormulas.bachelierOptionImpliedVolatility(forward, optionMaturity, optionStrike, payoffUnit, value);
+		}
+		else {
+			return convertFromTo(model, optionMaturity, optionStrike, convertFromTo(model, optionMaturity, optionStrike, value, fromQuotingConvention, QuotingConvention.PRICE), QuotingConvention.PRICE, toQuotingConvention);
+		}
+	}
+	/**
+	 * Convert the value of a caplet from on quoting convention to another quoting convention.
+	 * 
+	 * @param optionMaturity Option maturity of the caplet.
+	 * @param optionStrike Option strike of the caplet.
+	 * @param value Value of the caplet given in the form of <code>fromQuotingConvention</code>.
+	 * @param fromQuotingConvention The quoting convention of the given value.
+	 * @param toQuotingConvention The quoting convention requested.
+	 * @return Value of the caplet given in the form of <code>toQuotingConvention</code>. 
+	 */
+	public double convertFromTo(double optionMaturity, double optionStrike, double value, QuotingConvention fromQuotingConvention, QuotingConvention toQuotingConvention) {
+		return convertFromTo(null, optionMaturity, optionStrike, value, fromQuotingConvention, toQuotingConvention);
 	}
 }
