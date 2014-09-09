c91e0954b4dc9a77929ebef20a4f60d6bef1c92e
Christian Fries <email@christian-fries.de>
2014-09-09 15:02:41 +0000

    Added unit test to compare finite difference of integrated variance and analytic instantaneous volatility.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1271 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/test/java6/net/finmath/marketdata/model/volatilities/CapletVolatilitiesParametricTest.java
+++ src/test/java6/net/finmath/marketdata/model/volatilities/CapletVolatilitiesParametricTest.java
@@ -8,11 +8,15 @@ package net.finmath.marketdata.model.volatilities;
 
 import static org.junit.Assert.*;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 import net.finmath.marketdata.model.volatilities.VolatilitySurfaceInterface.QuotingConvention;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -20,6 +24,10 @@ import org.junit.Test;
  *
  */
 public class CapletVolatilitiesParametricTest {
+
+	private static DecimalFormat formatterMaturity	= new DecimalFormat("00.00", new DecimalFormatSymbols(Locale.ENGLISH));
+	private static DecimalFormat formatterValue		= new DecimalFormat("  ##0.000%;-##0.000%", new DecimalFormatSymbols(Locale.ENGLISH));
+	private static DecimalFormat formatterDeviation	= new DecimalFormat(" 0.00000E00;-0.00000E00", new DecimalFormatSymbols(Locale.ENGLISH));
 
 	@Test
 	public void testFlatVolatilityUsingD() {
@@ -58,5 +66,51 @@ public class CapletVolatilitiesParametricTest {
 		double volatility = volatilityModel.getValue(maturity, strike, QuotingConvention.VOLATILITYLOGNORMAL);
 		
 		assertTrue(volatility - a * Math.sqrt((1-Math.exp(- 2 * c * maturity)) / (2 * c * maturity)) < 1E-15);
+	}
+	
+	@Test
+	public void testIntegratedFourParameterExponentialVolatility() {
+
+		double a = 0.25;
+		double b = 3.00;
+		double c = 1.50;
+		double d = 0.10;
+
+		double eps = 1E-5;
+
+		System.out.println("Comparing finite difference differentiation of integrated variance with analytic value:\n");
+		System.out.println("  t  " + "\t" + " analytic " + "\t" + " finite diff " + "\t" + " deviation ");
+
+		Calendar referenceDate = new GregorianCalendar();
+		CapletVolatilitiesParametric volatilityModel = new CapletVolatilitiesParametric("volSurface", referenceDate, a, b, c, d);
+
+		double maxAbsDeviation = 0.0;
+		for(double t=0.01; t<=10; t += 0.01) {
+			
+			double volatilityUp = volatilityModel.getValue(t+eps, 0.0, QuotingConvention.VOLATILITYLOGNORMAL);
+			double volatilityDn = volatilityModel.getValue(t-eps, 0.0, QuotingConvention.VOLATILITYLOGNORMAL);
+			double integratedVarianceUp = volatilityUp*volatilityUp*(t+eps);
+			double integratedVarianceDn = volatilityDn*volatilityDn*(t-eps);
+			
+			double valueFiniteDifference = (integratedVarianceUp-integratedVarianceDn) / (2*eps);
+			double sigma = (a + b*t)*Math.exp(-c*t)+d;
+			double value = sigma*sigma;
+			double deviation = value-valueFiniteDifference;
+			
+			System.out.print(formatterMaturity.format(t) + "\t");
+			System.out.print(formatterValue.format(value) + "\t");
+			System.out.print(formatterValue.format(valueFiniteDifference) + "\t");
+			System.out.print(formatterDeviation.format(value-valueFiniteDifference) + "\n");
+
+			maxAbsDeviation = Math.max(maxAbsDeviation, Math.abs(deviation));
+		}
+
+		System.out.println("Maximum abs deviation: " + formatterDeviation.format(maxAbsDeviation));
+		System.out.println("__________________________________________________________________________________________\n");
+
+		/*
+		 * jUnit assertion: condition under which we consider this test successful
+		 */
+		Assert.assertTrue(Math.abs(maxAbsDeviation) < 1E-8);
 	}
 }
