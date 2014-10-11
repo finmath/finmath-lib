a574d41c308e30503f1f80618f1e7de1034902f6
Christian Fries <email@christian-fries.de>
2014-10-11 07:47:53 +0000

    Added minimum index and power index.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1327 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/montecarlo/interestrate/products/indices/MinIndex.java
@@ -0,0 +1,50 @@
+/*
+ * Created on 22.11.2009
+ */
+package net.finmath.montecarlo.interestrate.products.indices;
+
+import java.util.Arrays;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+import net.finmath.montecarlo.interestrate.products.components.AbstractProductComponent;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * A minumum index.
+ * 
+ * Provides the function min(index1(t), index2(t), ...).
+ * 
+ * @author Christian Fries
+ * @version 1.2
+ */
+public class MinIndex extends AbstractIndex {
+
+	private static final long serialVersionUID = -1512137372132830198L;
+
+	private final AbstractProductComponent[] indexArguments;
+
+    /**
+	 * Creates the function min(index1(t), index2(t), ...)
+	 * 
+     * @param indexArguments An arguments list of <code>AbstractProductComponent</code>s or an array thereof. A minimum number of 2 arguments is required.
+     */
+    public MinIndex(AbstractProductComponent... indexArguments) {
+	    super();
+	    if(indexArguments.length < 1) throw new IllegalArgumentException("Missing arguments. Please provide one or more arguments.");
+	    this.indexArguments = indexArguments;
+    }
+
+    @Override
+    public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {
+    	RandomVariableInterface value = indexArguments[0].getValue(evaluationTime, model);
+    	for(AbstractProductComponent index : indexArguments) value = value.cap(index.getValue(evaluationTime, model));
+    	return value;
+    }
+
+	@Override
+	public String toString() {
+		return "MinIndex [indexArguments=" + Arrays.toString(indexArguments)
+				+ "]";
+	}
+}
---
+++ src/main/java6/net/finmath/montecarlo/interestrate/products/indices/PowIndex.java
@@ -0,0 +1,47 @@
+/*
+ * Created on 22.11.2009
+ */
+package net.finmath.montecarlo.interestrate.products.indices;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+import net.finmath.montecarlo.interestrate.products.components.AbstractProductComponent;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * A power index.
+ * 
+ * Provides the function <i>index(t)<sup>exponent</sup></i>, i.e. pow(index(t), exponent).
+ * 
+ * @author Christian Fries
+ * @version 1.2
+ */
+public class PowIndex extends AbstractIndex {
+
+	private static final long serialVersionUID = -1512137372132830198L;
+
+	private final AbstractProductComponent	index;
+	private final double					exponent;
+
+    /**
+	 * Creates the function pow(index(t), exponent)
+	 * 
+     * @param index An index.
+     * @param exponent The exponent.
+     */
+    public PowIndex(AbstractProductComponent index, double exponent) {
+	    super();
+	    this.index = index;
+	    this.exponent = exponent;
+    }
+
+    @Override
+    public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {
+    	return index.getValue(evaluationTime, model).pow(exponent);
+    }
+
+	@Override
+	public String toString() {
+		return "PowIndex [index=" + index + ", exponent=" + exponent + "]";
+	}
+}
