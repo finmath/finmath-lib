c7463142529d1460dd551353980979724b5e3e47
Christian Fries <email@christian-fries.de>
2014-10-12 19:54:28 +0000

    Added forward curve index to repo.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1335 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java6/net/finmath/montecarlo/interestrate/products/indices/FowardCurveIndex.java
@@ -0,0 +1,44 @@
+/*
+ * Created on 12.10.2014
+ *
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ */
+package net.finmath.montecarlo.interestrate.products.indices;
+
+import net.finmath.marketdata.model.curves.ForwardCurveInterface;
+import net.finmath.montecarlo.RandomVariable;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * A fixed coupon index paying coupon calculated from a forward curve.
+ * 
+ * @see net.finmath.marketdata.model.curves.ForwardCurveInterface
+ * @author Christian Fries
+ */
+public class FowardCurveIndex extends AbstractIndex {
+
+	private static final long serialVersionUID = 5375406324063846793L;
+	
+	private final ForwardCurveInterface fowardCurve;
+	
+	/**
+	 * Creates a forward curve index.
+	 * 
+	 * @param fowardCurve The forward curve.
+	 */
+	public FowardCurveIndex(ForwardCurveInterface fowardCurve) {
+		super();
+		this.fowardCurve = fowardCurve;
+	}
+
+	@Override
+	public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) {
+		return new RandomVariable(fowardCurve.getForward(null,  evaluationTime));
+	}
+
+	@Override
+	public String toString() {
+		return "FowardCurveIndex [fowardCurve=" + fowardCurve + "]";
+	}
+}
