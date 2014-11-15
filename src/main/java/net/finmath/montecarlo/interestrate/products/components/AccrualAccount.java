89f9e1a43fc16947cf1114b10e5c54a4e05c4d25
Christian Fries <email@christian-fries.de>
2014-11-15 07:31:40 +0000

    Added accrual account (improved version of money market account).
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1397 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java/net/finmath/montecarlo/interestrate/products/components/AccrualAccount.java
@@ -0,0 +1,74 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 08.11.2014
+ */
+
+package net.finmath.montecarlo.interestrate.products.components;
+
+import java.util.Set;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.marketdata.model.curves.CurveInterface;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+import net.finmath.montecarlo.interestrate.products.MoneyMarketAccount;
+import net.finmath.montecarlo.interestrate.products.indices.AbstractIndex;
+import net.finmath.montecarlo.interestrate.products.indices.AnalyticModelIndex;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * Implementation of a general accrual account.
+ * 
+ * This class is an improved version of the {@link net.finmath.montecarlo.interestrate.product.MoneyMarketAccrount} except that it
+ * allow the use of a general index for accrual and also supports past fixings.
+ * 
+ * @author Christian Fries
+ *
+ */
+public class AccrualAccount extends AbstractProductComponent {
+
+	private static final long serialVersionUID = 188297603697240319L;
+
+	private AnalyticModelIndex	pastFixings		= null;
+	private AbstractIndex		accrualIndex;
+	private double				accrualPeriod;
+
+	/**
+	 * Create an accrual account.
+	 * 
+	 * @param currency The currency of this account.
+	 * @param pastFixings An analytic model index which is used for past fixings, i.e., all calls to getValue with evaluationTime < 0 are delegated to this index.
+	 * @param accrualIndex The accrual index.
+	 * @param accrualPeriod The accrual period.
+	 */
+	public AccrualAccount(String currency, AnalyticModelIndex pastFixings, AbstractIndex accrualIndex, double accrualPeriod) {
+		super(currency);
+		this.pastFixings	= pastFixings;
+		this.accrualIndex	= accrualIndex;
+		this.accrualPeriod	= accrualPeriod;
+	}
+
+	@Override
+	public Set<String> queryUnderlyings() {
+		return accrualIndex.queryUnderlyings();
+	}
+
+	@Override
+	public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {
+		if(evaluationTime <= 0) return pastFixings.getValue(evaluationTime, model);
+		
+		RandomVariableInterface value = pastFixings.getValue(0.0, model);
+		
+		// Loop over accrual periods
+		for(double time=0.0; time<evaluationTime; time += accrualPeriod) {	
+			// Get the forward fixed at the beginning of the period
+			RandomVariableInterface	forwardRate				= accrualIndex.getValue(time, model);
+			double					currentAccrualPeriod	= Math.min(accrualPeriod , evaluationTime-time);
+			
+			// Accrue the value using the current forward rate
+			value = value.accrue(forwardRate, currentAccrualPeriod);
+		}
+		
+		return value;
+	}
+}
