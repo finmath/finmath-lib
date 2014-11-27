dfa5dbb93e7eb153079b7981067191cafe98463c
Christian Fries <email@christian-fries.de>
2014-11-27 05:54:43 +0000

    Added product index.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1428 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/main/java6/net/finmath/montecarlo/interestrate/products/indices/ProductIndex.java
@@ -0,0 +1,54 @@
+/*
+ * Created on 03.09.2006
+ *
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ */
+package net.finmath.montecarlo.interestrate.products.indices;
+
+import java.util.Set;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+import net.finmath.montecarlo.interestrate.products.components.AbstractProductComponent;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * A product index being index1(t) * index2(t)
+ * 
+ * @author Christian Fries
+ */
+public class ProductIndex extends AbstractIndex {
+
+	private static final long serialVersionUID = -8181742829635380940L;
+
+	private AbstractProductComponent index1;
+	private AbstractProductComponent index2;
+
+	/**
+	 * Create a performance index being numeratorIndex(t) / denominatorIndex(t)
+	 * 
+	 * @param index1 First index.
+	 * @param index2 Second index.
+	 */
+	public ProductIndex(AbstractIndex index1, AbstractIndex index2) {
+		super();
+		this.index1	= index1;
+		this.index2	= index2;
+	}
+
+	@Override
+	public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {
+		return index1.getValue(evaluationTime, model).mult(index2.getValue(evaluationTime, model));
+	}
+
+	@Override
+	public Set<String> queryUnderlyings() {
+		Set<String> underlyingNames			= index1.queryUnderlyings();
+		Set<String>	underlyingNames2		= index2.queryUnderlyings();
+		if(underlyingNames2 != null) {
+			if(underlyingNames != null)	underlyingNames.addAll(underlyingNames2);
+			else						underlyingNames = underlyingNames2;
+		}
+		return underlyingNames;
+	}
+}
