6e1c78246a7578f59a7658a9d9d50d860d1db675
Christian Fries <email@christian-fries.de>
2015-02-19 22:11:27 +0000

    Added the Jarque-Bera test.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1504 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/test/java6/net/finmath/functions/JarqueBeraTestTest.java
@@ -0,0 +1,32 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 14.02.2015
+ */
+
+package net.finmath.functions;
+
+import net.finmath.montecarlo.BrownianMotion;
+import net.finmath.time.TimeDiscretization;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test, testing that the Brownian increment is normal distributed
+ * using the Jarque-Bera test.
+ * 
+ * @author Christian Fries
+ */
+public class JarqueBeraTestTest {
+
+	@Test
+	public void test() {
+		BrownianMotion bm = new BrownianMotion(new TimeDiscretization(0.0, 1.0, 2.0), 1 /* numberOfFactors */, 10000 /* numberOfPaths */, 2342 /* seed */);
+		
+		double test = (new JarqueBeraTest()).test(bm.getBrownianIncrement(0 /* timeIndex */, 0 /* factor */));
+		
+		Assert.assertTrue("Normal distributed (level of significance 0.10)", test < 4.6);
+	}
+
+}
