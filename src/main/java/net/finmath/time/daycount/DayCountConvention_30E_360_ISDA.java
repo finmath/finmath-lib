ede0a391097f82b6938be224ddef511db475a3ea
Christian Fries <email@christian-fries.de>
2015-06-13 22:05:16 +0000

    Offers a boolean whether to consider the endDate as a termination date (in which case last day of February is not mapped to 30).
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1616 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/main/java/net/finmath/time/daycount/DayCountConvention_30E_360_ISDA.java
+++ src/main/java/net/finmath/time/daycount/DayCountConvention_30E_360_ISDA.java
@@ -13,19 +13,33 @@ import java.util.Calendar;
  * 
  * Calculates the day count as
  * <code>
- *		(endDateYear - startDateYear) * 360.0 + (endDateMonth - startDateMonth) * 30.0 + (Math.min(endDateDay, startDateDay &gt; 29 ? 30.0 : 31.0) - Math.min(startDateDay, 30.0))
+ * 		(endDateYear - startDateYear) * 360.0 + (endDateMonth - startDateMonth) * 30.0 + (Math.min(endDateDay,30) - Math.min(startDateDay, 30.0));
  * </code>
+ * where for endDateDay is the to 30, if endDate is not the termination date and the last day of February.
+
+ * The constructor offers a boolean to decide if endDate is treated as a termination date or not.
+ * A termination date is the endDate of the last period in a schedule.
  * 
  * @author Christian Fries
  */
 public class DayCountConvention_30E_360_ISDA implements DayCountConventionInterface {
 
+	private final boolean isTreatEndDateAsTerminationDate;
+	
+	/**
+	 * Create a 30E/360 ISDA daycount convention.
+	 */
+	public DayCountConvention_30E_360_ISDA(boolean isTreatEndDateAsTerminationDate) {
+		this.isTreatEndDateAsTerminationDate = isTreatEndDateAsTerminationDate;
+	}
+
 	/**
 	 * Create a 30E/360 ISDA daycount convention.
 	 */
 	public DayCountConvention_30E_360_ISDA() {
+		this(false);
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see net.finmath.time.daycount.DayCountConventionInterface#getDaycount(java.util.Calendar, java.util.Calendar)
 	 */
@@ -36,12 +50,21 @@ public class DayCountConvention_30E_360_ISDA implements DayCountConventionInterf
 		double startDateDay 	= startDate.get(Calendar.DAY_OF_MONTH);
 		double startDateMonth 	= startDate.get(Calendar.MONTH);
 		double startDateYear 	= startDate.get(Calendar.YEAR);
-		
+
 		double endDateDay 		= endDate.get(Calendar.DAY_OF_MONTH);
 		double endDateMonth 	= endDate.get(Calendar.MONTH);
 		double endDateYear 		= endDate.get(Calendar.YEAR);
-		
-		return (endDateYear - startDateYear) * 360.0 + (endDateMonth - startDateMonth) * 30.0 + (Math.min(endDateDay, startDateDay > 29 ? 30.0 : 31.0) - Math.min(startDateDay, 30.0));
+
+
+		// Check if we have last day of February
+		boolean isStartDateLastDayOfFebruary = (startDateMonth == 1 && startDateDay == startDate.getActualMaximum(Calendar.DAY_OF_MONTH));
+		boolean isEndDateLastDayOfFebruary = (endDateMonth == 1 && endDateDay == endDate.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+		// Last day of February and 31st of a month are both treated as "30".
+		if(isStartDateLastDayOfFebruary || startDateDay == 31) startDateDay = 30;
+		if((isEndDateLastDayOfFebruary && !isTreatEndDateAsTerminationDate) || endDateDay == 31) endDateDay = 30;
+
+		return (endDateYear - startDateYear) * 360.0 + (endDateMonth - startDateMonth) * 30.0 + (endDateDay - Math.min(startDateDay, 30.0));
 	}
 
 	/* (non-Javadoc)
