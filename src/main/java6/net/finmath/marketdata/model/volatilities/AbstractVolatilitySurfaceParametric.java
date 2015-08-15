87546e711450c005a8e74e182fd213d75437a8e4
Christian Fries <email@christian-fries.de>
2015-08-15 12:19:10 +0000

    Calibration method now supports optimizer factory.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1652 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/main/java6/net/finmath/marketdata/model/volatilities/AbstractVolatilitySurfaceParametric.java
+++ src/main/java6/net/finmath/marketdata/model/volatilities/AbstractVolatilitySurfaceParametric.java
@@ -20,6 +20,7 @@ import net.finmath.marketdata.calibration.ParameterTransformation;
 import net.finmath.marketdata.calibration.Solver;
 import net.finmath.marketdata.model.AnalyticModelInterface;
 import net.finmath.marketdata.products.AnalyticProductInterface;
+import net.finmath.optimizer.OptimizerFactoryInterface;
 import net.finmath.optimizer.SolverException;
 
 /**
@@ -45,18 +46,21 @@ public abstract class AbstractVolatilitySurfaceParametric extends AbstractVolati
 	}
 
 	public AbstractVolatilitySurfaceParametric getCloneCalibrated(final AnalyticModelInterface calibrationModel, final Vector<AnalyticProductInterface> calibrationProducts, final List<Double> calibrationTargetValues, Map<String,Object> calibrationParameters, final ParameterTransformation parameterTransformation) throws CalculationException, SolverException {
+		return getCloneCalibrated(calibrationModel, calibrationProducts, calibrationTargetValues, calibrationParameters, parameterTransformation, null);
+	}
+
+	public AbstractVolatilitySurfaceParametric getCloneCalibrated(final AnalyticModelInterface calibrationModel, final Vector<AnalyticProductInterface> calibrationProducts, final List<Double> calibrationTargetValues, Map<String,Object> calibrationParameters, final ParameterTransformation parameterTransformation, OptimizerFactoryInterface optimizerFactory) throws CalculationException, SolverException {
 		if(calibrationParameters == null) calibrationParameters = new HashMap<String,Object>();
 		Integer maxIterationsParameter	= (Integer)calibrationParameters.get("maxIterations");
 		Double	accuracyParameter		= (Double)calibrationParameters.get("accuracy");
 		Double	evaluationTimeParameter		= (Double)calibrationParameters.get("evaluationTime");
 
-		// @TODO: These constants should become parameters. The numberOfPaths and seed is only relevant if Monte-Carlo products are used for calibration.
-		int maxIterations	= maxIterationsParameter != null ? maxIterationsParameter.intValue() : 400;
+		int maxIterations	= maxIterationsParameter != null ? maxIterationsParameter.intValue() : 600;
 		double accuracy		= accuracyParameter != null ? accuracyParameter.doubleValue() : 1E-8;
 		double evaluationTime		= evaluationTimeParameter != null ? evaluationTimeParameter.doubleValue() : 0.0;
 
 		AnalyticModelInterface model = calibrationModel.addVolatilitySurfaces(this);
-		Solver solver = new Solver(model, calibrationProducts, calibrationTargetValues, parameterTransformation, evaluationTime, accuracy);
+		Solver solver = new Solver(model, calibrationProducts, calibrationTargetValues, parameterTransformation, evaluationTime, optimizerFactory);
 
 		Set<ParameterObjectInterface> objectsToCalibrate = new HashSet<ParameterObjectInterface>();
 		objectsToCalibrate.add(this);
