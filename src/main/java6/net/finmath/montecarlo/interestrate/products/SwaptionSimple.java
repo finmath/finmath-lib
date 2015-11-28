1013ace1a791410686307fce01068d1bd62d7323
Christian Fries <email@christian-fries.de>
2015-11-28 10:02:08 +0000

    Cleanup.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1726 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/main/java6/net/finmath/montecarlo/interestrate/products/SwaptionSimple.java
+++ src/main/java6/net/finmath/montecarlo/interestrate/products/SwaptionSimple.java
@@ -24,78 +24,75 @@ import net.finmath.time.TimeDiscretizationInterface;
  */
 public class SwaptionSimple extends AbstractLIBORMonteCarloProduct {
 
-    public enum ValueUnit {
-        VALUE,
-        INTEGRATEDVARIANCE,
-        VOLATILITY
-    }
+	public enum ValueUnit {
+		VALUE,
+		INTEGRATEDVARIANCE,
+		VOLATILITY
+	}
 
-    private final TimeDiscretizationInterface	tenor;
-    private final double						swaprate;
-    private final Swaption						swaption;
-    private final ValueUnit						valueUnit;
+	private final TimeDiscretizationInterface	tenor;
+	private final double						swaprate;
+	private final Swaption						swaption;
+	private final ValueUnit						valueUnit;
 
-    /**
-     * Note: It is implicitly assumed that swapTenor[0] is the exercise date (no forward starting).
-     * @param swaprate The strike swaprate of the swaption.
-     * @param swapTenor The swap tenor in doubles.
-     */
-    public SwaptionSimple(double swaprate, TimeDiscretizationInterface swapTenor) {
-        this(swaprate, swapTenor.getAsDoubleArray(), ValueUnit.VALUE);
-    }
-
-    /**
-     * Note: It is implicitly assumed that swapTenor[0] is the exercise date (no forward starting).
-     * @param swaprate The strike swaprate of the swaption.
-     * @param swapTenor The swap tenor in doubles.
-     * @param valueUnit See <code>getValue(AbstractLIBORMarketModel model)</code>
-     */
-    public SwaptionSimple(double swaprate, double[] swapTenor, ValueUnit valueUnit) {
-        super();
-        this.tenor = new TimeDiscretization(swapTenor);
-        this.swaprate = swaprate;
-        this.swaption	= new Swaption(swapTenor[0], tenor, swaprate);
-        this.valueUnit	= valueUnit;
-    }
-    
-    /**
-     * This method returns the value random variable of the product within the specified model, evaluated at a given evalutationTime.
-     * Note: For a lattice this is often the value conditional to evalutationTime, for a Monte-Carlo simulation this is the (sum of) value discounted to evaluation time.
-     * Cashflows prior evaluationTime are not considered.
-     * 
-     * @param evaluationTime The time on which this products value should be observed.
-     * @param model The model used to price the product.
-     * @return The random variable representing the value of the product discounted to evaluation time
-     * @throws net.finmath.exception.CalculationException Thrown if the valuation fails, specific cause may be available via the <code>cause()</code> method.
-     */
-    @Override
-    public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {
-    	RandomVariableInterface value = swaption.getValue(evaluationTime, model);
-
-    	if(valueUnit == ValueUnit.VALUE) return value;
-
-    	ForwardCurveInterface forwardCurve	= model.getModel().getForwardRateCurve();
-    	ForwardCurveInterface discountCurve	= forwardCurve;
-
-    	double parSwaprate = Swap.getForwardSwapRate(tenor, tenor, forwardCurve);
-    	double optionMaturity = tenor.getTime(0);
-    	double strikeSwaprate = swaprate;
-    	double swapAnnuity = SwapAnnuity.getSwapAnnuity(tenor, discountCurve);
-    	
-    	double volatility = AnalyticFormulas.blackScholesOptionImpliedVolatility(parSwaprate, optionMaturity, strikeSwaprate, swapAnnuity, value.getAverage());
-    	
-    	if(valueUnit == ValueUnit.VOLATILITY) {
-    		return model.getRandomVariableForConstant(volatility);
-    	} else if(valueUnit == ValueUnit.INTEGRATEDVARIANCE) {
-    		return model.getRandomVariableForConstant(volatility * volatility * optionMaturity);
-    	}
-    	
-    	throw new UnsupportedOperationException("Provided valueUnit not implemented.");
-    }
-
-    /* (non-Javadoc)
-	 * @see java.lang.Object#toString()
+	/**
+	 * Note: It is implicitly assumed that swapTenor[0] is the exercise date (no forward starting).
+	 * @param swaprate The strike swaprate of the swaption.
+	 * @param swapTenor The swap tenor in doubles.
 	 */
+	public SwaptionSimple(double swaprate, TimeDiscretizationInterface swapTenor) {
+		this(swaprate, swapTenor.getAsDoubleArray(), ValueUnit.VALUE);
+	}
+
+	/**
+	 * Note: It is implicitly assumed that swapTenor[0] is the exercise date (no forward starting).
+	 * @param swaprate The strike swaprate of the swaption.
+	 * @param swapTenor The swap tenor in doubles.
+	 * @param valueUnit See <code>getValue(AbstractLIBORMarketModel model)</code>
+	 */
+	public SwaptionSimple(double swaprate, double[] swapTenor, ValueUnit valueUnit) {
+		super();
+		this.tenor = new TimeDiscretization(swapTenor);
+		this.swaprate = swaprate;
+		this.swaption	= new Swaption(swapTenor[0], tenor, swaprate);
+		this.valueUnit	= valueUnit;
+	}
+
+	/**
+	 * This method returns the value random variable of the product within the specified model, evaluated at a given evalutationTime.
+	 * Note: For a lattice this is often the value conditional to evalutationTime, for a Monte-Carlo simulation this is the (sum of) value discounted to evaluation time.
+	 * Cashflows prior evaluationTime are not considered.
+	 * 
+	 * @param evaluationTime The time on which this products value should be observed.
+	 * @param model The model used to price the product.
+	 * @return The random variable representing the value of the product discounted to evaluation time
+	 * @throws net.finmath.exception.CalculationException Thrown if the valuation fails, specific cause may be available via the <code>cause()</code> method.
+	 */
+	@Override
+	public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {
+		RandomVariableInterface value = swaption.getValue(evaluationTime, model);
+
+		if(valueUnit == ValueUnit.VALUE) return value;
+
+		ForwardCurveInterface forwardCurve	= model.getModel().getForwardRateCurve();
+		ForwardCurveInterface discountCurve	= forwardCurve;
+
+		double parSwaprate = Swap.getForwardSwapRate(tenor, tenor, forwardCurve);
+		double optionMaturity = tenor.getTime(0);
+		double strikeSwaprate = swaprate;
+		double swapAnnuity = SwapAnnuity.getSwapAnnuity(tenor, discountCurve);
+
+		double volatility = AnalyticFormulas.blackScholesOptionImpliedVolatility(parSwaprate, optionMaturity, strikeSwaprate, swapAnnuity, value.getAverage());
+
+		if(valueUnit == ValueUnit.VOLATILITY) {
+			return model.getRandomVariableForConstant(volatility);
+		} else if(valueUnit == ValueUnit.INTEGRATEDVARIANCE) {
+			return model.getRandomVariableForConstant(volatility * volatility * optionMaturity);
+		}
+
+		throw new UnsupportedOperationException("Provided valueUnit not implemented.");
+	}
+
 	@Override
 	public String toString() {
 		return "SwaptionSimple [tenor=" + tenor + ", swaprate=" + swaprate
