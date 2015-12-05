7d258f6fdc5e4eac5025891acd95b38e8882ae80
Christian Fries <email@christian-fries.de>
2015-12-05 21:12:09 +0000

    Fixed Java 8 compatiblity.
    git-svn-id: http://svn.finmath.net/finmath%20lib/trunk@1744 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/test/java/net/finmath/time/ScheduleGeneratorTest.java
+++ src/test/java/net/finmath/time/ScheduleGeneratorTest.java
@@ -6,7 +6,8 @@
 
 package net.finmath.time;
 
-import org.joda.time.LocalDate;
+import java.time.LocalDate;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -33,9 +34,9 @@ public class ScheduleGeneratorTest {
 	@Test
 	public void test() {
 		ScheduleInterface schedule = ScheduleGenerator.createScheduleFromConventions(
-				new LocalDate(2012, 1, 10) /* referenceDate */,
-				new LocalDate(2012, 10, 12) /* startDate */,
-				new LocalDate(2013, 1, 12) /* maturity */,
+				LocalDate.of(2012, 1, 10) /* referenceDate */,
+				LocalDate.of(2012, 10, 12) /* startDate */,
+				LocalDate.of(2013, 1, 12) /* maturity */,
 				Frequency.QUARTERLY,
 				DaycountConvention.ACT_360,
 				ShortPeriodConvention.FIRST,
@@ -47,7 +48,7 @@ public class ScheduleGeneratorTest {
 		System.out.println(schedule);
 
 		ScheduleInterface schedule2 = ScheduleGenerator.createScheduleFromConventions(
-				new LocalDate(2012, 1, 10) /* referenceDate */,
+				LocalDate.of(2012, 1, 10) /* referenceDate */,
 				"9M 2D" /* startOffset */,
 				"3M" /* maturity */,
 				"quarterly" /* frequency */,
@@ -62,12 +63,12 @@ public class ScheduleGeneratorTest {
 			Assert.assertTrue(schedule2.getPeriods().contains(period));
 		}
 
-		Assert.assertTrue("Period start.", schedule2.getPeriod(0).getPeriodStart().equals(new LocalDate(2012, 1+9, 10+2)));
+		Assert.assertTrue("Period start.", schedule2.getPeriod(0).getPeriodStart().equals(LocalDate.of(2012, 1+9, 10+2)));
 		
 		/*
 		 * 12.01.2013 is a saturday. End date rolls to 14.01.2013
 		 */
-		Assert.assertTrue("Period end.", schedule2.getPeriod(0).getPeriodEnd().equals(new LocalDate(2013, 01, 14)));
+		Assert.assertTrue("Period end.", schedule2.getPeriod(0).getPeriodEnd().equals(LocalDate.of(2013, 01, 14)));
 		System.out.println(schedule2);
 	}
 
