4a66c90f98e11a3e61d998f90582fd672d0a1b30
Christian Fries <email@christian-fries.de>
2016-12-06 08:27:12 +0100

    Added backward compatibility to java-6 version of finmath lib.

--- src/main/java/net/finmath/time/ScheduleGenerator.java
+++ src/main/java/net/finmath/time/ScheduleGenerator.java
@@ -8,6 +8,7 @@ package net.finmath.time;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
 import net.finmath.time.businessdaycalendar.BusinessdayCalendar;
 import net.finmath.time.businessdaycalendar.BusinessdayCalendarAny;
