457c4cf8befd6a09e3c6700f3d2ee1b98bcc950e
Christian Fries <email@christian-fries.de>
2017-05-14 15:52:13 +0200

    Corrected indentation.

--- src/main/java/net/finmath/marketdata/model/curves/ForwardCurveInterface.java
+++ src/main/java/net/finmath/marketdata/model/curves/ForwardCurveInterface.java
@@ -21,28 +21,28 @@ public interface ForwardCurveInterface extends CurveInterface {
 	 * 
 	 * @return The forward.
 	 */
-    double getForward(AnalyticModelInterface model, double fixingTime);
-    
+	double getForward(AnalyticModelInterface model, double fixingTime);
+
 	/**
 	 * Returns the forward for the corresponding fixing time and paymentOffset.
 	 * @param model An analytic model providing a context. Some curves do not need this (can be null).
 	 * @param fixingTime The fixing time of the index associated with this forward curve.
-     * @param paymentOffset The payment offset (as internal day count fraction) specifying the payment of this index. Used only as a fallback and/or consistency check.
+	 * @param paymentOffset The payment offset (as internal day count fraction) specifying the payment of this index. Used only as a fallback and/or consistency check.
 	 * 
 	 * @return The forward.
 	 */
-    double getForward(AnalyticModelInterface model, double fixingTime, double paymentOffset);
+	double getForward(AnalyticModelInterface model, double fixingTime, double paymentOffset);
 
-    /**
+	/**
 	 * @return The name of the discount curve associated with this forward curve (e.g. OIS for collateralized forwards)
 	 */
 	String getDiscountCurveName();
-	
+
 	/**
 	 * Returns the payment offset associated with this forward curve and a corresponding fixingTime.
 	 * 
 	 * @param fixingTime The fixing time of the index associated with this forward curve.
 	 * @return The payment offset associated with this forward curve.
 	 */
-    double getPaymentOffset(double fixingTime);
+	double getPaymentOffset(double fixingTime);
 }
