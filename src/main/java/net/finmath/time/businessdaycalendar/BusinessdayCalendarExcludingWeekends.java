3b15018639e646e8fc252fcb87bb19bc71bd91ab
Christian Fries <email@christian-fries.de>
2017-05-19 23:40:37 +0200

    Improved output of toString.

--- src/main/java/net/finmath/time/businessdaycalendar/BusinessdayCalendarExcludingWeekends.java
+++ src/main/java/net/finmath/time/businessdaycalendar/BusinessdayCalendarExcludingWeekends.java
@@ -43,7 +43,8 @@ public class BusinessdayCalendarExcludingWeekends extends BusinessdayCalendar {
 			&& date.getDayOfWeek() != DayOfWeek.SUNDAY;  
 	}
 
+	@Override
 	public String toString() {
-		return "BusinessdayCalendarExcludingWeekends";
+		return "BusinessdayCalendarExcludingWeekends [baseCalendar=" + baseCalendar + "]";
 	}
 }
