2f5fef5d96afdd8e9e3eb26b5efa9cca7a8eb495
Christian Fries <email@christian-fries.de>
2017-06-14 23:02:01 +0200

    Introducing BusinessdayCalendarExcludingGivenHolidays
    A base class that can be used to implement more specific holiday
    calendars based on a Set of holidays.

---
+++ src/main/java/net/finmath/time/businessdaycalendar/BusinessdayCalendarExcludingGivenHolidays.java
@@ -0,0 +1,63 @@
+package net.finmath.time.businessdaycalendar;
+
+import java.util.Set;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+/**
+ * A business day calendar, where every day is a business day, except for weekends and New York City holidays
+ * 
+ * @author Christian Fries
+ */
+public abstract class BusinessdayCalendarGivenHolidays extends BusinessdayCalendar {
+
+	private final String name;
+	private final BusinessdayCalendarInterface baseCalendar;
+	private final boolean isExcludeWeekends;
+	
+	public BusinessdayCalendarGivenHolidays(String name, BusinessdayCalendarInterface baseCalendar, boolean isExcludeWeekends) {
+		super();
+		this.name = name;
+		this.baseCalendar = baseCalendar;
+		this.isExcludeWeekends = isExcludeWeekends;
+	}
+
+	/**
+	 * @return A name of the calendar (e.g. to identify the calendar).
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @return A base calendar, if any. May be null.
+	 */
+	public BusinessdayCalendarInterface getBaseCalendar() {
+		return baseCalendar;
+	}
+
+	/**
+	 * @return If true, DayOfWeek.SATURDAY and  DayOfWeek.SUNDAY are excluded (no-business days)
+	 */
+	public boolean isExcludingWeekends() { return isExcludeWeekends; }
+
+	/**
+	 * @return A set of (additional) holidays.
+	 */
+	public abstract Set<LocalDate> getHolidays();
+
+	@Override
+	public boolean isBusinessday(LocalDate date) {
+		return	(baseCalendar == null || baseCalendar.isBusinessday(date))
+				&& (isExcludingWeekends()
+						&& date.getDayOfWeek() != DayOfWeek.SATURDAY
+						&& date.getDayOfWeek() != DayOfWeek.SUNDAY
+						)
+				&& !getHolidays().contains(date);  
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " [baseCalendar=" + getBaseCalendar() + "]";
+	}
+}
