8d4e11452e2baa4b19ed89bbfd2eb2a715c3c440
Christian Fries <email@christian-fries.de>
2017-07-04 20:03:34 +0200

    Added getIncrement(int timeIndex) with default implementation.

--- src/main/java/net/finmath/montecarlo/IndependentIncrementsInterface.java
+++ src/main/java/net/finmath/montecarlo/IndependentIncrementsInterface.java
@@ -26,6 +26,25 @@ public interface IndependentIncrementsInterface {
 	/**
 	 * Return the increment for a given timeIndex.
 	 * 
+	 * The method returns the random variablevector
+	 *	 <i>&Delta; X(t<sub>i</sub>) := X(t<sub>i+1</sub>)-X(t<sub>i</sub>)</i>
+	 * for the given time index <i>i</i>.
+	 * 
+	 * @param timeIndex The time index (corresponding to the this class's time discretization)
+	 * @return The factor (component) of the increments (a random variable)
+	 */
+	default RandomVariableInterface[] getIncrement(int timeIndex)
+	{
+		RandomVariableInterface[] increment = new RandomVariableInterface[getNumberOfFactors()];
+		for(int factorIndex = 0; factorIndex<getNumberOfFactors(); factorIndex++) {
+			increment[factorIndex] = getIncrement(timeIndex, factorIndex);
+		}
+		return increment;
+	}
+
+	/**
+	 * Return the increment for a given timeIndex and given factor.
+	 * 
 	 * The method returns the random variable
 	 *	 <i>&Delta; X<sub>j</sub>(t<sub>i</sub>) := X<sub>j</sub>(t<sub>i+1</sub>)-X(t<sub>i</sub>)</i>
 	 * for the given time index <i>i</i> and a given factor (index) <i>j</i>
