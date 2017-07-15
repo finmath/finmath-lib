b8c8f7a756078cb8ec6cf34e8ac113ee71f3f2e1
Christian Fries <email@christian-fries.de>
2017-07-15 13:49:04 +0200

    Clean up.

--- src/main/java/net/finmath/timeseries/models/parametric/DisplacedLognormalARMAGARCH.java
+++ src/main/java/net/finmath/timeseries/models/parametric/DisplacedLognormalARMAGARCH.java
@@ -243,7 +243,7 @@ public class DisplacedLognormalARMAGARCH implements TimeSeriesModelParametric, H
 		// Seek optimal parameter configuration
 		OptimizerInterface lm = new LevenbergMarquardt(guessParameters, new double[] { 1000.0 }, maxIterations*100, 2) {
 			private static final long serialVersionUID = -8844232820888815090L;
-			
+
 			@Override
 			public void setValues(double[] arg0, double[] arg1) throws SolverException {
 				arg1[0] = objectiveFunction.value(arg0);
