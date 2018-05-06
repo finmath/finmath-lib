4225ce6606005a96d8580450c73cb4766278c4d5
Christian Fries <email@christian-fries.de>
2018-05-06 15:30:17 +0200

    Unit tests are parameterized with different RandomVariableFactory.

--- src/test/java/net/finmath/montecarlo/BrownianMotionTest.java
+++ src/test/java/net/finmath/montecarlo/BrownianMotionTest.java
@@ -6,9 +6,14 @@
 package net.finmath.montecarlo;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import net.finmath.functions.JarqueBeraTest;
 import net.finmath.stochastic.RandomVariableInterface;
@@ -19,11 +24,28 @@ import net.finmath.time.TimeDiscretizationInterface;
  * @author Christian Fries
  * 
  */
+@RunWith(Parameterized.class)
 public class BrownianMotionTest {
 
 	static final DecimalFormat formatterReal2	= new DecimalFormat(" 0.00");
 	static final DecimalFormat formatterSci4	= new DecimalFormat(" 0.0000E00;-0.0000E00");
 	static final DecimalFormat formatterSci1	= new DecimalFormat(" 0E00;-0.E00");
+
+	private AbstractRandomVariableFactory randomVariableFactory;
+
+	@Parameters(name="{0}")
+	public static Collection<Object[]> generateData()
+	{
+		return Arrays.asList(new Object[][] {
+			{ new RandomVariableFactory(true /* isUseDoublePrecisionFloatingPointImplementation */)},
+			{ new RandomVariableFactory(false /* isUseDoublePrecisionFloatingPointImplementation */)},
+		});
+	};
+
+	public BrownianMotionTest(AbstractRandomVariableFactory randomVariableFactory) {
+		super();
+		this.randomVariableFactory = randomVariableFactory;
+	}
 
 	@Test
 	public void testScalarValuedBrownianMotionTerminalDistribution() {
@@ -44,7 +66,8 @@ public class BrownianMotionTest {
 					timeDiscretization,
 					1,
 					numberOfPaths,
-					seed
+					seed,
+					randomVariableFactory
 			);
 			
 			System.out.print("\tNumber of path = " + formatterSci1.format(numberOfPaths) + "\t ");
@@ -83,7 +106,8 @@ public class BrownianMotionTest {
 				timeDiscretization,
 				1,
 				numberOfPaths,
-				seed
+				seed,
+				randomVariableFactory
 		);
 		
 		JarqueBeraTest jb = new JarqueBeraTest();
@@ -126,7 +150,8 @@ public class BrownianMotionTest {
 				timeDiscretization,
 				2,
 				numberOfPaths,
-				seed
+				seed,
+				randomVariableFactory
 		);
 		
 		System.out.println("Test of average and variance of the integral of (Delta W)^2.");
--- src/test/java6/net/finmath/montecarlo/RandomVariableTest.java
+++ src/test/java6/net/finmath/montecarlo/RandomVariableTest.java
@@ -5,9 +5,15 @@
  */
 package net.finmath.montecarlo;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.apache.commons.math3.random.MersenneTwister;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import net.finmath.stochastic.RandomVariableInterface;
 
@@ -17,24 +23,43 @@ import net.finmath.stochastic.RandomVariableInterface;
  * @author Christian Fries
  * @see net.finmath.montecarlo.RandomVariable
  */
+@RunWith(Parameterized.class)
 public class RandomVariableTest {
+
+	private AbstractRandomVariableFactory randomVariableFactory;
+
+	@Parameters(name="{0}")
+	public static Collection<Object[]> generateData()
+	{
+		return Arrays.asList(new Object[][] {
+			{ new RandomVariableFactory(true /* isUseDoublePrecisionFloatingPointImplementation */)},
+			{ new RandomVariableFactory(false /* isUseDoublePrecisionFloatingPointImplementation */)},
+			{ new RandomVariableLazyEvaluationFactory()}
+		});
+	};
+
+	public RandomVariableTest(AbstractRandomVariableFactory randomVariableFactory) {
+		super();
+		this.randomVariableFactory = randomVariableFactory;
+	}
+
 
 	@Test
 	public void testRandomVariableDeterministc() {
 
 		// Create a random variable with a constant
-		RandomVariableInterface randomVariable = new RandomVariable(2.0);
-		
+		RandomVariableInterface randomVariable = randomVariableFactory.createRandomVariable(2.0);
+
 		// Perform some calculations
 		randomVariable = randomVariable.mult(2.0);
 		randomVariable = randomVariable.add(1.0);
 		randomVariable = randomVariable.squared();
 		randomVariable = randomVariable.sub(4.0);
 		randomVariable = randomVariable.div(7.0);
-		
+
 		// The random variable has average value 3.0 (it is constant 3.0)
 		Assert.assertTrue(randomVariable.getAverage() == 3.0);
-		
+
 		// Since the random variable is deterministic, it has zero variance
 		Assert.assertTrue(randomVariable.getVariance() == 0.0);
 	}
@@ -43,23 +68,23 @@ public class RandomVariableTest {
 	public void testRandomVariableStochastic() {
 
 		// Create a stochastic random variable
-		RandomVariableInterface randomVariable2 = new RandomVariable(0.0,
+		RandomVariableInterface randomVariable2 = randomVariableFactory.createRandomVariable(0.0,
 				new double[] {-4.0, -2.0, 0.0, 2.0, 4.0} );
 
 		// Perform some calculations
 		randomVariable2 = randomVariable2.add(4.0);
 		randomVariable2 = randomVariable2.div(2.0);
-		
+
 		// The random variable has average value 2.0
 		Assert.assertTrue(randomVariable2.getAverage() == 2.0);
 
 		// The random variable has variance value 2.0 = (4 + 1 + 0 + 1 + 4) / 5
 		Assert.assertEquals(2.0, randomVariable2.getVariance(), 1E-12);
-		
+
 		// Multiply two random variables, this will expand the receiver to a stochastic one
 		RandomVariableInterface randomVariable = new RandomVariable(3.0);
 		randomVariable = randomVariable.mult(randomVariable2);
-		
+
 		// The random variable has average value 6.0
 		Assert.assertTrue(randomVariable.getAverage() == 6.0);
 
@@ -71,11 +96,11 @@ public class RandomVariableTest {
 	public void testRandomVariableArithmeticSqrtPow() {
 
 		// Create a stochastic random variable
-		RandomVariableInterface randomVariable = new RandomVariable(0.0,
+		RandomVariableInterface randomVariable = randomVariableFactory.createRandomVariable(0.0,
 				new double[] {3.0, 1.0, 0.0, 2.0, 4.0, 1.0/3.0} );
 
 		RandomVariableInterface check = randomVariable.sqrt().sub(randomVariable.pow(0.5));
-		
+
 		// The random variable is identical 0.0
 		Assert.assertTrue(check.getAverage() == 0.0);
 		Assert.assertTrue(check.getVariance() == 0.0);
@@ -85,11 +110,11 @@ public class RandomVariableTest {
 	public void testRandomVariableArithmeticSquaredPow() {
 
 		// Create a stochastic random variable
-		RandomVariableInterface randomVariable = new RandomVariable(0.0,
+		RandomVariableInterface randomVariable = randomVariableFactory.createRandomVariable(0.0,
 				new double[] {3.0, 1.0, 0.0, 2.0, 4.0, 1.0/3.0} );
 
 		RandomVariableInterface check = randomVariable.squared().sub(randomVariable.pow(2.0));
-		
+
 		// The random variable is identical 0.0
 		Assert.assertTrue(check.getAverage() == 0.0);
 		Assert.assertTrue(check.getVariance() == 0.0);
@@ -99,32 +124,32 @@ public class RandomVariableTest {
 	public void testRandomVariableStandardDeviation() {
 
 		// Create a stochastic random variable
-		RandomVariableInterface randomVariable = new RandomVariable(0.0,
+		RandomVariableInterface randomVariable = randomVariableFactory.createRandomVariable(0.0,
 				new double[] {3.0, 1.0, 0.0, 2.0, 4.0, 1.0/3.0} );
 
 		double check = randomVariable.getStandardDeviation() - Math.sqrt(randomVariable.getVariance());
 		Assert.assertTrue(check == 0.0);
 	}
-	
+
 	/**
 	 * Testing quantiles of normal distribution.
 	 * Based on feedback provided by Alessandro Gnoatto and a student of him.
 	 */
 	@Test
 	public void testGetQuantile() {
-		
+
 		final int seed = 3141;
 		final int numberOfSamplePoints = 10000000;
-		
+
 		MersenneTwister mersenneTwister = new MersenneTwister(seed);
 		double[] samples = new double[numberOfSamplePoints];
 		for(int i = 0; i< numberOfSamplePoints; i++) {
 			double randomNumber = mersenneTwister.nextDouble();
 			samples[i] = net.finmath.functions.NormalDistribution.inverseCumulativeDistribution(randomNumber);
 		}
-		
-		RandomVariableInterface normalDistributedRandomVariable = new RandomVariable(0.0,samples);
-		
+
+		RandomVariableInterface normalDistributedRandomVariable = randomVariableFactory.createRandomVariable(0.0,samples);
+
 		double q00 = normalDistributedRandomVariable.getQuantile(0.0);
 		Assert.assertEquals(normalDistributedRandomVariable.getMin(), q00, 1E-12);
 
