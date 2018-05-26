2c08c1ea3e0d61d0ea977928c8954ec3ae1f4f3b
Christian Fries <email@christian-fries.de>
2018-05-26 17:14:25 +0200

    Added analytic approximation for swaption under normal LMM.

---
+++ src/test/java/net/finmath/montecarlo/interestrate/products/SwaptionNormalTest.java
@@ -0,0 +1,225 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. Contact: email@christian-fries.de.
+ *
+ * Created on 28 Feb 2015
+ */
+package net.finmath.montecarlo.interestrate.products;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.functions.AnalyticFormulas;
+import net.finmath.marketdata.model.curves.DiscountCurveFromForwardCurve;
+import net.finmath.marketdata.model.curves.ForwardCurve;
+import net.finmath.marketdata.model.curves.ForwardCurveInterface;
+import net.finmath.montecarlo.BrownianMotionInterface;
+import net.finmath.montecarlo.interestrate.LIBORMarketModel;
+import net.finmath.montecarlo.interestrate.LIBORMarketModel.CalibrationItem;
+import net.finmath.montecarlo.interestrate.LIBORMarketModelInterface;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulation;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+import net.finmath.montecarlo.interestrate.modelplugins.AbstractLIBORCovarianceModelParametric;
+import net.finmath.montecarlo.interestrate.modelplugins.LIBORCorrelationModelExponentialDecay;
+import net.finmath.montecarlo.interestrate.modelplugins.LIBORCovarianceModelExponentialForm7Param;
+import net.finmath.montecarlo.interestrate.modelplugins.LIBORCovarianceModelFromVolatilityAndCorrelation;
+import net.finmath.montecarlo.interestrate.modelplugins.LIBORVolatilityModel;
+import net.finmath.montecarlo.interestrate.modelplugins.LIBORVolatilityModelFourParameterExponentialForm;
+import net.finmath.montecarlo.interestrate.modelplugins.LIBORVolatilityModelFromGivenMatrix;
+import net.finmath.montecarlo.interestrate.products.AbstractLIBORMonteCarloProduct;
+import net.finmath.montecarlo.interestrate.products.Bond;
+import net.finmath.montecarlo.interestrate.products.DigitalCaplet;
+import net.finmath.montecarlo.interestrate.products.SimpleSwap;
+import net.finmath.montecarlo.interestrate.products.Swaption;
+import net.finmath.montecarlo.interestrate.products.SwaptionAnalyticApproximation;
+import net.finmath.montecarlo.process.ProcessEulerScheme;
+import net.finmath.time.TimeDiscretization;
+import net.finmath.time.TimeDiscretizationInterface;
+
+/**
+ * This class tests the LIBOR market model and products.
+ * 
+ * @author Christian Fries
+ * @author Lorenzo Torricelli
+ */
+public class SwaptionNormalTest {
+
+	private static DecimalFormat formatterMaturity	= new DecimalFormat("00.00", new DecimalFormatSymbols(Locale.ENGLISH));
+	private static DecimalFormat formatterValue		= new DecimalFormat(" ##0.000%;-##0.000%", new DecimalFormatSymbols(Locale.ENGLISH));
+	private static DecimalFormat formatterDeviation	= new DecimalFormat(" 0.00000E00;-0.00000E00", new DecimalFormatSymbols(Locale.ENGLISH));
+
+	private final LIBORModelMonteCarloSimulationInterface liborModel;
+
+	private final int numberOfPaths		= 20000;
+	private final int numberOfFactors	= 6;  //PCA number of factors
+	private final double correlationDecayParam = 0.2;
+
+	public SwaptionNormalTest() throws CalculationException {
+		liborModel = SwaptionNormalTest.createLIBORMarketModel(numberOfPaths, numberOfFactors, correlationDecayParam);
+	}
+
+	@Test
+	public void testSwaption() throws CalculationException {
+		/*
+		 * Value a swaption
+		 */
+		System.out.println("Swaption prices:\n");
+		System.out.println("Maturity      Simulation       Analytic         Bachelier Vol    Error");
+
+		// Create libor Market model
+		for (int maturityIndex = 1; maturityIndex <= liborModel.getNumberOfLibors() - 10; maturityIndex++) {
+			double exerciseDate = liborModel.getLiborPeriod(maturityIndex);
+			System.out.print(formatterMaturity.format(exerciseDate) + "          ");
+
+			int numberOfPeriods = 5;
+
+			// Create a swaption
+
+			double[] fixingDates = new double[numberOfPeriods];
+			double[] paymentDates = new double[numberOfPeriods]; //simply a merge of fixing and payment dates (which obviously overlap)
+			double[] swapTenor = new double[numberOfPeriods + 1];   //to be passed to the analytical approximation method
+			double swapPeriodLength = 0.5; //Use instead -> liborMarketModel.getLiborTimediscretisation to make the libor discretisation
+			//coincide with the swap time discretisation
+
+			for (int periodStartIndex = 0; periodStartIndex < numberOfPeriods; periodStartIndex++) {
+				fixingDates[periodStartIndex] = exerciseDate + periodStartIndex * swapPeriodLength;
+				paymentDates[periodStartIndex] = exerciseDate + (periodStartIndex + 1) * swapPeriodLength;
+				swapTenor[periodStartIndex] = exerciseDate + periodStartIndex * swapPeriodLength;
+			}
+			swapTenor[numberOfPeriods] = exerciseDate + numberOfPeriods * swapPeriodLength;
+
+			// Swaptions swap rate
+			double swaprate = 0.02;  //getParSwaprate(liborModel, swapTenor);
+
+			// Set swap rates for each period
+			double[] swaprates = new double[numberOfPeriods];
+			for (int periodStartIndex = 0; periodStartIndex < numberOfPeriods; periodStartIndex++) {
+				swaprates[periodStartIndex] = swaprate;
+			}
+
+
+			// Value with Monte Carlo
+			Swaption swaptionMonteCarlo	= new Swaption(exerciseDate, fixingDates, paymentDates, swaprates);
+			double valueSimulation = swaptionMonteCarlo.getValue(liborModel);
+			System.out.print(formatterValue.format(valueSimulation) + "          ");
+
+			//Insert time here
+
+			// Value of the analytic approximation
+			SwaptionGeneralizedAnalyticApproximation swaptionAnalytic = new SwaptionGeneralizedAnalyticApproximation(swaprate, swapTenor, SwaptionGeneralizedAnalyticApproximation.ValueUnit.VALUE,
+					SwaptionGeneralizedAnalyticApproximation.StateSpace.NORMAL); 
+			SwaptionGeneralizedAnalyticApproximation swaptionBachelierVol = new SwaptionGeneralizedAnalyticApproximation(swaprate, swapTenor, SwaptionGeneralizedAnalyticApproximation.ValueUnit.VOLATILITY,
+					SwaptionGeneralizedAnalyticApproximation.StateSpace.NORMAL); 
+
+
+			double valueAnalytic = swaptionAnalytic.getValue(liborModel);
+			System.out.print(formatterValue.format(valueAnalytic) + "          ");
+
+			double valueVol= swaptionBachelierVol.getValue(liborModel);
+			System.out.print(formatterValue.format(valueVol) + "          ");
+
+			// Absolute error
+			double deviation = Math.abs(valueSimulation - valueAnalytic);
+			System.out.println(formatterDeviation.format(deviation) + "          ");
+			
+			Assert.assertEquals(valueSimulation, valueAnalytic, 1E-3);
+		}
+	}
+
+	public static LIBORModelMonteCarloSimulationInterface createLIBORMarketModel(int numberOfPaths, int numberOfFactors, double correlationDecayParam) throws CalculationException {
+
+		/*
+		 * Create the libor tenor structure and the initial values
+		 */
+		double liborPeriodLength	= 0.5;
+		double liborRateTimeHorzion	= 20.0;
+		TimeDiscretization liborPeriodDiscretization = new TimeDiscretization(0.0, (int) (liborRateTimeHorzion / liborPeriodLength), liborPeriodLength);
+
+		// Create the forward curve (initial value of the LIBOR market model)
+		ForwardCurve forwardCurve = ForwardCurve.createForwardCurveFromForwards(
+				"forwardCurve"								/* name of the curve */,
+				new double[] {0.5 , 1.0 , 2.0 , 5.0 , 40.0}	/* fixings of the forward */,
+				new double[] {0.02, 0.02, 0.02, 0.02, 0.02}	/* forwards */,
+				liborPeriodLength							/* tenor / period length */
+				);
+
+		/*
+		 * Create a simulation time discretization
+		 */
+		double lastTime	= 20.0;
+		double dt		= 0.5;
+
+		TimeDiscretization timeDiscretization = new TimeDiscretization(0.0, (int) (lastTime / dt), dt);
+
+		/*
+		 * Create a volatility structure v[i][j] = sigma_j(t_i)
+		 */
+		double[][] volatility = new double[timeDiscretization.getNumberOfTimeSteps()][liborPeriodDiscretization.getNumberOfTimeSteps()];
+		for (int timeIndex = 0; timeIndex < volatility.length; timeIndex++) {
+			for (int liborIndex = 0; liborIndex < volatility[timeIndex].length; liborIndex++) {
+				// Create a very simple volatility model here
+				double time = timeDiscretization.getTime(timeIndex);
+				double maturity = liborPeriodDiscretization.getTime(liborIndex);
+				double timeToMaturity = maturity - time;
+
+				double instVolatility;
+				if(timeToMaturity <= 0)
+					instVolatility = 0;				// This forward rate is already fixed, no volatility
+				else
+					instVolatility = (0.3 + 0.2 * Math.exp(-0.25 * timeToMaturity))
+					* forwardCurve.getForward(null, liborPeriodDiscretization.getTime(liborIndex)); //rescale by the interest rate level; not necessary, but tidier/more realistic values
+
+				// Store
+				volatility[timeIndex][liborIndex] = instVolatility;
+			}
+		}
+		LIBORVolatilityModelFromGivenMatrix volatilityModel = new LIBORVolatilityModelFromGivenMatrix(timeDiscretization, liborPeriodDiscretization, volatility);
+		//			LIBORVolatilityModel volatilityModel = new LIBORVolatilityModelFourParameterExponentialForm(timeDiscretization, liborPeriodDiscretization, 0.00, 0.0, 0.0, 0.01, false);
+
+		/*
+		 * Create a correlation model rho_{i,j} = exp(-a * abs(T_i-T_j))
+		 */
+		LIBORCorrelationModelExponentialDecay correlationModel = new LIBORCorrelationModelExponentialDecay(
+				timeDiscretization, liborPeriodDiscretization, numberOfFactors,
+				correlationDecayParam);
+
+
+		/*
+		 * Combine volatility model and correlation model to a covariance model
+		 */
+		LIBORCovarianceModelFromVolatilityAndCorrelation covarianceModel =
+				new LIBORCovarianceModelFromVolatilityAndCorrelation(timeDiscretization,
+						liborPeriodDiscretization, volatilityModel, correlationModel);
+
+		// Set model properties
+		Map<String, String> properties = new HashMap<String, String>();
+
+		// Choose the simulation measure
+		properties.put("measure", LIBORMarketModel.Measure.SPOT.name());
+
+		// Choose log normal model
+		properties.put("stateSpace", LIBORMarketModel.StateSpace.NORMAL.name());
+
+		// Empty array of calibration items - hence, model will use given covariance
+		LIBORMarketModel.CalibrationItem[] calibrationItems = new LIBORMarketModel.CalibrationItem[0];
+
+		/*
+		 * Create corresponding LIBOR Market Model
+		 */
+		LIBORMarketModelInterface liborMarketModel = new LIBORMarketModel(liborPeriodDiscretization, forwardCurve, new DiscountCurveFromForwardCurve(forwardCurve), covarianceModel, calibrationItems, properties);
+
+		BrownianMotionInterface brownianMotion = new net.finmath.montecarlo.BrownianMotion(timeDiscretization, numberOfFactors, numberOfPaths, 3141 /* seed */);
+
+		ProcessEulerScheme process = new ProcessEulerScheme(brownianMotion, ProcessEulerScheme.Scheme.PREDICTOR_CORRECTOR);
+
+		return new LIBORModelMonteCarloSimulation(liborMarketModel, process);
+	}
+}
