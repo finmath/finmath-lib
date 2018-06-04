b138d0a026e51aa22f0d3e0eb4332fa5f754bab9
Christian Fries <email@christian-fries.de>
2018-06-04 23:23:39 +0200

    Moved unit test to test folder.

---
+++ src/test/java/net/finmath/marketdata/model/curves/locallinearregression/CurveEstimationTest.java
@@ -0,0 +1,75 @@
+/*
+ * (c) Copyright finmath.net, Germany. Contact: info@finmath.net.
+ *
+ * Created on 01.06.2018
+ */
+package net.finmath.marketdata.model.curves.locallinearregression;
+
+import java.time.LocalDate;
+
+import org.apache.commons.math3.distribution.AbstractRealDistribution;
+import org.apache.commons.math3.distribution.NormalDistribution;
+import org.jblas.DoubleMatrix;
+import org.junit.Assert;
+import org.junit.Test;
+
+import net.finmath.marketdata.model.curves.CurveInterface;
+
+public class CurveEstimationTest {
+
+	/**
+	 * Simple test of curve consisting of two points..
+	 */
+	@Test
+	public void testLinearInterpolation() {
+		LocalDate date=LocalDate.now();
+		double[] X = { 0.0 , 1.0 };
+		double[] Y = { 1.0 , 0.8 };
+		double bandwidth = 1500;
+
+		CurveEstimation estimatedcurve = new CurveEstimation(date, bandwidth, X, Y, X, 0.5);
+		CurveInterface regressionCurve = estimatedcurve.getRegressionCurve();
+
+		Assert.assertEquals("left extrapolatoin", 1.0, regressionCurve.getValue(-0.5), 1E-12);
+		Assert.assertEquals("left interpolation", 0.95, regressionCurve.getValue(0.25), 1E-12);
+		Assert.assertEquals("center interpolation", 0.9, regressionCurve.getValue(0.50), 1E-12);
+		Assert.assertEquals("right interpolation", 0.85, regressionCurve.getValue(0.75), 1E-12);
+		Assert.assertEquals("right extrapolatoin", 0.8, regressionCurve.getValue(1.5), 1E-12);
+
+
+		AbstractRealDistribution kernel=new NormalDistribution();
+		double K=kernel.density(0.0);
+		DoubleMatrix R=new DoubleMatrix(new double[] {K*(Y[0]+Y[1]),Y[1]*(X[1]-X[0])*K} );
+		DoubleMatrix M=new DoubleMatrix(new double[][] {{2*K,K*(X[1]-X[0])},{K*(X[1]-X[0]),K*(X[1]-X[0])*(X[1]-X[0])}} );
+		double detM= M.get(0,0)*M.get(1, 1)-M.get(1,0)*M.get(1,0);
+		DoubleMatrix MInv=new DoubleMatrix(new double[][] {{M.get(1, 1),-M.get(1,0)},{-M.get(1,0),M.get(0,0)}} );
+		MInv=MInv.mul(1/detM);
+		DoubleMatrix a=MInv.mmul(R);
+		System.out.println(R.toString());
+		System.out.println(M.toString());
+		System.out.println(a.toString());
+	}
+
+
+	/**
+	 * Regression matrix (currently no test, just for inspection)
+	 */
+	@Test
+	public void testRegressionMatrix() {
+		double[] X = { 0.0 , 1.0 };
+		double[] Y = { 1.0 , 0.8 };
+
+
+		AbstractRealDistribution kernel=new NormalDistribution();
+		double K=kernel.density(0.0);
+		DoubleMatrix R=new DoubleMatrix(new double[] {K*(Y[0]+Y[1]),Y[1]*(X[1]-X[0])*K} );
+		DoubleMatrix M=new DoubleMatrix(new double[][] {{2*K,K*(X[1]-X[0])},{K*(X[1]-X[0]),K*(X[1]-X[0])*(X[1]-X[0])}} );
+		double detM= M.get(0,0)*M.get(1, 1)-M.get(1,0)*M.get(1,0);
+		DoubleMatrix MInv=new DoubleMatrix(new double[][] {{M.get(1, 1),-M.get(1,0)},{-M.get(1,0),M.get(0,0)}} );
+		MInv=MInv.mul(1/detM);
+		DoubleMatrix a=MInv.mmul(R);
+		System.out.println(R.toString());
+		System.out.println(M.toString());
+		System.out.println(a.toString());
+	}
+}
