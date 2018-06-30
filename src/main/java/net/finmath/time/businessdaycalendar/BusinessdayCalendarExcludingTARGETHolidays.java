3f7d5e23d011aca83dc598b55d67b5f648dd3a3d
Christian Fries <email@christian-fries.de>
2018-06-30 23:15:55 +0200

    Fixed style issues.

--- src/main/java/net/finmath/time/businessdaycalendar/BusinessdayCalendarExcludingTARGETHolidays.java
+++ src/main/java/net/finmath/time/businessdaycalendar/BusinessdayCalendarExcludingTARGETHolidays.java
@@ -64,10 +64,10 @@ public class BusinessdayCalendarExcludingTARGETHolidays extends BusinessdayCalen
 	 * @return True, if date is easter sunday.
 	 */
 	public static boolean isEasterSunday(LocalDate date) {
-	    int Y = date.getYear();
-	    int a = Y % 19;
-	    int b = Y / 100;
-	    int c = Y % 100;
+	    int y = date.getYear();
+	    int a = y % 19;
+	    int b = y / 100;
+	    int c = y % 100;
 	    int d = b / 4;
 	    int e = b % 4;
 	    int f = (b + 8) / 25;
@@ -75,10 +75,10 @@ public class BusinessdayCalendarExcludingTARGETHolidays extends BusinessdayCalen
 	    int h = (19 * a + b - d - g + 15) % 30;
 	    int i = c / 4;
 	    int k = c % 4;
-	    int L = (32 + 2 * e + 2 * i - h - k) % 7;
-	    int m = (a + 11 * h + 22 * L) / 451;
-	    int easterSundayMonth	= (h + L - 7 * m + 114) / 31;
-	    int easterSundayDay		= ((h + L - 7 * m + 114) % 31) + 1;
+	    int l = (32 + 2 * e + 2 * i - h - k) % 7;
+	    int m = (a + 11 * h + 22 * l) / 451;
+	    int easterSundayMonth	= (h + l - 7 * m + 114) / 31;
+	    int easterSundayDay		= ((h + l - 7 * m + 114) % 31) + 1;
 
 		int month = date.getMonthValue();
 		int day = date.getDayOfMonth();
