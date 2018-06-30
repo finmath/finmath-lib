f731ac5cf9fe135ffe5e181d75787f26e2c11017
Christian Fries <email@christian-fries.de>
2018-06-30 21:39:40 +0200

    Added package-info.

---
+++ src/main/java/net/finmath/montecarlo/crosscurrency/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides classes for Cross-Currency models to be implemented via Monte-Carlo
+ * algorithms from <code>net.finmath.montecarlo.process</code>.
+ * 
+ * @author Christian Fries
+ */
+package net.finmath.montecarlo.crosscurrency;
