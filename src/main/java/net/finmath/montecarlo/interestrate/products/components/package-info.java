79a31df693fe0a25086466320a823a9e86813792
Christian Fries <email@christian-fries.de>
2018-07-06 18:42:12 +0200

    Improved package description.

---
+++ src/main/java/net/finmath/montecarlo/interestrate/products/components/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides a set product components which allow to build financial products by composition.
+ * 
+ * @author Christian Fries
+ */
+package net.finmath.montecarlo.interestrate.products.components;
---
+++ src/main/java/net/finmath/montecarlo/interestrate/products/indices/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides a set of indices which can be used as part of a period.
+ * 
+ * @see net.finmath.montecarlo.interestrate.products.components.Period
+ * @author Christian Fries
+ */
+package net.finmath.montecarlo.interestrate.products.indices;
