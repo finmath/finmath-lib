776486659f16b684bdb79fa6cf346ebf3e7da4d9
Christian Fries <email@christian-fries.de>
2018-07-07 10:19:02 +0200

    Added Halton sequence for Java 6 branch.

---
+++ src/main/java6/net/finmath/randomnumbers/RandomNumberGenerator.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package net.finmath.randomnumbers;
+
+/**
+ * Interface for an n-dimensional random number generator
+ * generating a sequence of vectors sampling the space [0,1]^{n}
+ * 
+ * @author Christian Fries
+ */
+public interface RandomNumberGenerator {
+
+	double[] getNext();
+	
+	int getDimension();
+}
