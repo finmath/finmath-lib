fae7dd6c24d23832a723cd31126a543f48d406f3
Christian Fries <email@christian-fries.de>
2018-07-07 10:18:26 +0200

    Added JavaDoc.

--- src/main/java/net/finmath/randomnumbers/HaltonSequence.java
+++ src/main/java/net/finmath/randomnumbers/HaltonSequence.java
@@ -5,6 +5,11 @@
  */
 package net.finmath.randomnumbers;
 
+/**
+ * Implements a multi-dimensional Halton sequence (quasi random numbers) with the given bases.
+ * 
+ * @author Christian Fries
+ */
 public class HaltonSequence implements RandomNumberGenerator {
 
 	private final int base[];
@@ -12,9 +17,11 @@ public class HaltonSequence implements RandomNumberGenerator {
 	private int currentIndex = 0;
 
 	/**
-	 * Constructs a Halton sequence with the given base.
+	 * Constructs a Halton sequence with the given bases.
 	 * 
-	 * @param base
+	 * The bases should be integers without common divisor greater than 1, for example, prime numbers.
+	 * 
+	 * @param base The array of base integers. The length of the array defines the dimension of the sequence.
 	 */
 	public HaltonSequence(int[] base) {
 		for(int i=0; i<base.length; i++) {
@@ -47,10 +54,10 @@ public class HaltonSequence implements RandomNumberGenerator {
 	}
 
 	/**
-	 * Return a Halton number, sequence starting at index = 0, base >= 2.
+	 * Return a Halton number, sequence starting at index = 0, base &gt; 1.
 	 * 
 	 * @param index The index of the sequence.
-	 * @param base The base of the sequence.
+	 * @param base The base of the sequence. Has to be greater than one (this is not checked).
 	 * @return The Halton number.
 	 */
 	public static double getHaltonNumberForGivenBase(long index, int base) {
