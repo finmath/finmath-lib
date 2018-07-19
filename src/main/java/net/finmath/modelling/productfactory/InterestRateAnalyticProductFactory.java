27d274721d515a8b167e9c2f88b1d66fbba3b8ef
Christian Fries <email@christian-fries.de>
2018-07-19 23:32:07 +0300

    Fixed style issues.

--- src/main/java/net/finmath/modelling/productfactory/InterestRateAnalyticProductFactory.java
+++ src/main/java/net/finmath/modelling/productfactory/InterestRateAnalyticProductFactory.java
@@ -11,17 +11,17 @@ import net.finmath.modelling.descriptor.InterestRateSwapLegProductDescriptor;
 import net.finmath.modelling.descriptor.InterestRateSwapProductDescriptor;
 
 /**
- * Product factory of interest rate derivatives for use with an analytic model. 
+ * Product factory of interest rate derivatives for use with an analytic model.
  *
  * @author Christian Fries
  * @author Roland Bachl
  */
 public class InterestRateAnalyticProductFactory implements ProductFactory<InterestRateProductDescriptor> {
-	
+
 	private final String				forwardCurveName;
 	private final String				discountCurveName;
 	private final String				discountCurveForNotionalResetName;
-	
+
 
 	public InterestRateAnalyticProductFactory(String forwardCurveName, String discountCurveName,
 			String discountCurveForNotionalResetName) {
@@ -30,23 +30,23 @@ public class InterestRateAnalyticProductFactory implements ProductFactory<Intere
 		this.discountCurveName = discountCurveName;
 		this.discountCurveForNotionalResetName = discountCurveForNotionalResetName;
 	}
-	
+
 	@Override
 	public DescribedProduct<? extends InterestRateProductDescriptor> getProductFromDescriptor(ProductDescriptor descriptor) {
 		if(descriptor instanceof InterestRateSwapLegProductDescriptor) {
 			InterestRateSwapLegProductDescriptor swapLeg = (InterestRateSwapLegProductDescriptor) descriptor;
-			DescribedProduct<InterestRateSwapLegProductDescriptor> product = new SwapLeg(swapLeg.getLegSchedule(), forwardCurveName, swapLeg.getSpread(), discountCurveName, 
+			DescribedProduct<InterestRateSwapLegProductDescriptor> product = new SwapLeg(swapLeg.getLegSchedule(), forwardCurveName, swapLeg.getSpread(), discountCurveName,
 					discountCurveForNotionalResetName, swapLeg.isNotionalExchanged());
 			return product;
 		}else if(descriptor instanceof InterestRateSwapProductDescriptor){
 			InterestRateSwapProductDescriptor swap = (InterestRateSwapProductDescriptor) descriptor;
 			// TODO what if these are not SwapLegs? Is that a realistic case? Do recursion?
 			InterestRateSwapLegProductDescriptor legDescriptor = (InterestRateSwapLegProductDescriptor) swap.getLegReceiver();
-//			AnalyticProductInterface legReceiver =  new SwapLeg(legDescriptor.getLegSchedule(), forwardCurveName, legDescriptor.getSpread(), discountCurveName, 
-//					discountCurveForNotionalResetName, legDescriptor.isNotionalExchanged());
+			//			AnalyticProductInterface legReceiver =  new SwapLeg(legDescriptor.getLegSchedule(), forwardCurveName, legDescriptor.getSpread(), discountCurveName,
+			//					discountCurveForNotionalResetName, legDescriptor.isNotionalExchanged());
 			AnalyticProductInterface legReceiver =  (AnalyticProductInterface) getProductFromDescriptor(legDescriptor);
 			legDescriptor = (InterestRateSwapLegProductDescriptor) swap.getLegPayer();
-			AnalyticProductInterface legPayer =  new SwapLeg(legDescriptor.getLegSchedule(), forwardCurveName, legDescriptor.getSpread(), discountCurveName, 
+			AnalyticProductInterface legPayer =  new SwapLeg(legDescriptor.getLegSchedule(), forwardCurveName, legDescriptor.getSpread(), discountCurveName,
 					discountCurveForNotionalResetName, legDescriptor.isNotionalExchanged());
 			DescribedProduct<InterestRateSwapProductDescriptor> product = new Swap(legReceiver, legPayer);
 			return product;
