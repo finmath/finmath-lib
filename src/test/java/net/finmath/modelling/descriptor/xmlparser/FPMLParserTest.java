2eec2816d41d1bfb31cf67126a406861943bb07c
Christian Fries <email@christian-fries.de>
2018-10-13 21:28:52 +0200

    Made unit test parametrized. User promt is shown when run through main()

--- src/test/java/net/finmath/modelling/descriptor/xmlparser/FPMLParserTest.java
+++ src/test/java/net/finmath/modelling/descriptor/xmlparser/FPMLParserTest.java
@@ -1,9 +1,12 @@
 package net.finmath.modelling.descriptor.xmlparser;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -11,25 +14,51 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.xml.sax.SAXException;
 
 import net.finmath.modelling.descriptor.InterestRateSwapLegProductDescriptor;
 import net.finmath.modelling.descriptor.InterestRateSwapProductDescriptor;
 
+@RunWith(Parameterized.class)
 public class FPMLParserTest {
 
-	static private File file;
+	@Parameters(name="file={0}}")
+	public static Collection<Object[]> generateData()
+	{
+		/// @TODO Provide a list of test files here
+		ArrayList<Object[]> parameters = new ArrayList<>();
+		parameters.add(new Object[] { new File("test.xml") });
+		return parameters;
+	}
 
-	@BeforeClass
-	public static void getFile() {
+	private final File file;
 
+	/**
+	 * This main method will prompt the user for a test file an run the test with the given file.
+	 *
+	 * @param args Arguments - not used.
+	 * @throws ParserConfigurationException 
+	 * @throws IOException 
+	 * @throws SAXException 
+	 */
+	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException
+	{
 		JFileChooser jfc = new JFileChooser(System.getProperty("user.home"));
 		jfc.setDialogTitle("Choose XML");
-		jfc.setFileFilter(new FileNameExtensionFilter("FpML (.xml)", "xml"));
+		jfc.setFileFilter(new FileNameExtensionFilter("FIPXML (.xml)", "xml"));
 		if(jfc.showOpenDialog(null) != JFileChooser.APPROVE_OPTION) {
 			System.exit(1);
 		};
-		file = jfc.getSelectedFile();
+
+		(new FPMLParserTest(jfc.getSelectedFile())).testGetSwapProductDescriptor();
+	}
+
+	public FPMLParserTest(File file) {
+		super();
+		this.file = file;
 	}
 
 	@Test
@@ -42,6 +71,9 @@ public class FPMLParserTest {
 		} catch (IllegalArgumentException e) {
 			System.out.println("There was a problem with the file: "+e.getMessage());
 			//			e.printStackTrace();
+			return;
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found. We will exit gracefully.");
 			return;
 		}
 
@@ -64,5 +96,4 @@ public class FPMLParserTest {
 		System.out.println(Arrays.toString(legPayer.getSpreads()));
 		System.out.println(legPayer.getLegScheduleDescriptor());
 	}
-
 }
