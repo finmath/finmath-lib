03267f564425cf60fc1dfd22b4c8740687d458f6
Christian Fries <email@christian-fries.de>
2018-10-13 18:31:51 +0200

    Added package-info to comply with style guide.

---
+++ src/main/java/net/finmath/modelling/describedproducts/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Classes providing the link/mapping from product descriptors to implementations.
+ * This is done by routing a descriptor to the appropriate constructor.
+ *
+ * @author Christian Fries
+ * @author Roland Bachl
+ */
+package net.finmath.modelling.describedproducts;
