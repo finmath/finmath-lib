53fcfcbd70ebfbdb156d64b34fa7062f14d37347
IkeThermite <theralph.rudd@gmail.com>
2018-11-01 11:39:13 +0200

    Implemented FDM European Put option and added tests.

--- src/test/java/net/finmath/finitedifference/BlackScholesThetaTest.java
+++ src/test/java/net/finmath/finitedifference/BlackScholesThetaTest.java
@@ -2,6 +2,7 @@ package net.finmath.finitedifference;
 
 import java.util.Arrays;
 
+import net.finmath.finitedifference.products.FDMEuropeanPutOption;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,7 +15,7 @@ import net.finmath.functions.AnalyticFormulas;
 public class BlackScholesThetaTest {
 
 	@Test
-	public void test() throws AssertionError {
+	public void testEuropeanCallOption() throws AssertionError {
 		double riskFreeRate = 0.06;
 		double volatility = 0.4;
 		double optionMaturity = 1;
@@ -22,8 +23,6 @@ public class BlackScholesThetaTest {
 
 		int numTimesteps = 35;
 		int numSpacesteps = 120;
-//		int numTimesteps = 3;
-//		int numSpacesteps = 3;
 		int numStandardDeviations = 5;
 		double initialValue = 50;
 		double theta = 0.5;
@@ -37,19 +36,56 @@ public class BlackScholesThetaTest {
 				initialValue,
 				riskFreeRate,
 				volatility);
-
 		FiniteDifference1DProduct callOption = new FDMEuropeanCallOption(optionMaturity, optionStrike);
-		double[][] valueFDM = callOption.getValue(0.0, model);
-		double[] initialStockPrice = valueFDM[0];
-		double[] optionValue = valueFDM[1];
-		double[] analyticalOptionValue = new double[optionValue.length];
-		for (int i =0; i < analyticalOptionValue.length; i++) {
-			analyticalOptionValue[i] = AnalyticFormulas.blackScholesOptionValue(initialStockPrice[i], riskFreeRate,
+
+		double[][] valueCallFDM = callOption.getValue(0.0, model);
+		double[] initialStockPriceForCall = valueCallFDM[0];
+		double[] callOptionValue = valueCallFDM[1];
+		double[] analyticalCallOptionValue = new double[callOptionValue.length];
+		for (int i =0; i < analyticalCallOptionValue.length; i++) {
+			analyticalCallOptionValue[i] = AnalyticFormulas.blackScholesOptionValue(initialStockPriceForCall[i], riskFreeRate,
 					volatility, optionMaturity, optionStrike, true);
 		}
-		System.out.println(Arrays.toString(optionValue));
-		Assert.assertArrayEquals(optionValue, analyticalOptionValue, 1e-2);
+		System.out.println(Arrays.toString(callOptionValue));
+		Assert.assertArrayEquals(callOptionValue, analyticalCallOptionValue, 1e-2);
+
 
 	}
+
+	@Test
+	public void testEuropeanPutOption() throws AssertionError {
+		double riskFreeRate = 0.06;
+		double volatility = 0.4;
+		double optionMaturity = 1;
+		double optionStrike = 50;
+
+		int numTimesteps = 35;
+		int numSpacesteps = 120;
+		int numStandardDeviations = 5;
+		double initialValue = 50;
+		double theta = 0.5;
+
+		FiniteDifference1DModel model = new FDMBlackScholesModel(
+				numTimesteps,
+				numSpacesteps,
+				numStandardDeviations,
+				optionStrike, // center of the grid.
+				theta,
+				initialValue,
+				riskFreeRate,
+				volatility);
+		FiniteDifference1DProduct putOption = new FDMEuropeanPutOption(optionMaturity, optionStrike);
+		double[][] valuePutFDM = putOption.getValue(0.0, model);
+		double[] initialStockPriceForPut = valuePutFDM[0];
+		double[] putOptionValue = valuePutFDM[1];
+		double[] analyticalPutOptionValue = new double[putOptionValue.length];
+		for (int i =0; i < analyticalPutOptionValue.length; i++) {
+			analyticalPutOptionValue[i] = AnalyticFormulas.blackScholesOptionValue(initialStockPriceForPut[i], riskFreeRate,
+					volatility, optionMaturity, optionStrike, false);
+		}
+		System.out.println(Arrays.toString(putOptionValue));
+		Assert.assertArrayEquals(putOptionValue, analyticalPutOptionValue, 1e-2);
+	}
+
 
 }
--- src/test/java/net/finmath/finitedifference/ConstantElasticityOfVarianceThetaTest.java
+++ src/test/java/net/finmath/finitedifference/ConstantElasticityOfVarianceThetaTest.java
@@ -3,6 +3,7 @@ package net.finmath.finitedifference;
 import net.finmath.finitedifference.models.FDMConstantElasticityOfVarianceModel;
 import net.finmath.finitedifference.models.FiniteDifference1DModel;
 import net.finmath.finitedifference.products.FDMEuropeanCallOption;
+import net.finmath.finitedifference.products.FDMEuropeanPutOption;
 import net.finmath.finitedifference.products.FiniteDifference1DProduct;
 import net.finmath.functions.AnalyticFormulas;
 import org.junit.Assert;
@@ -13,7 +14,7 @@ import java.util.Arrays;
 public class ConstantElasticityOfVarianceThetaTest {
 
     @Test
-    public void test() throws AssertionError {
+    public void testEuropeanCallOption() throws AssertionError {
         double riskFreeRate = 0.06;
         double volatility = 0.4;
         double exponent = 0.9;
@@ -45,6 +46,44 @@ public class ConstantElasticityOfVarianceThetaTest {
         for (int i =0; i < analyticalOptionValue.length; i++) {
             analyticalOptionValue[i] = AnalyticFormulas.constantElasticityOfVarianceOptionValue(initialStockPrice[i], riskFreeRate,
                     volatility, exponent, optionMaturity, optionStrike, true);
+        }
+        System.out.println(Arrays.toString(optionValue));
+        Assert.assertArrayEquals(analyticalOptionValue, optionValue, 5e-3);
+    }
+
+    @Test
+    public void testEuropeanPutOption() throws AssertionError {
+        double riskFreeRate = 0.06;
+        double volatility = 0.4;
+        double exponent = 0.9;
+        double optionMaturity = 1;
+        double optionStrike = 100;
+
+        int numTimesteps = 80;
+        int numSpacesteps = 160;
+        int numStandardDeviations = 5;
+        double initialValue = 100;
+        double theta = 0.5;
+
+        FiniteDifference1DModel model = new FDMConstantElasticityOfVarianceModel(
+                numTimesteps,
+                numSpacesteps,
+                numStandardDeviations,
+                optionStrike, // center of the grid.
+                theta,
+                initialValue,
+                riskFreeRate,
+                volatility,
+                exponent);
+
+        FiniteDifference1DProduct putOption = new FDMEuropeanPutOption(optionMaturity, optionStrike);
+        double[][] valueFDM = putOption.getValue(0.0, model);
+        double[] initialStockPrice = valueFDM[0];
+        double[] optionValue = valueFDM[1];
+        double[] analyticalOptionValue = new double[optionValue.length];
+        for (int i =0; i < analyticalOptionValue.length; i++) {
+            analyticalOptionValue[i] = AnalyticFormulas.constantElasticityOfVarianceOptionValue(initialStockPrice[i], riskFreeRate,
+                    volatility, exponent, optionMaturity, optionStrike, false);
         }
         System.out.println(Arrays.toString(optionValue));
         Assert.assertArrayEquals(analyticalOptionValue, optionValue, 5e-3);
