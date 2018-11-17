53bf3f95fabcb7e3ba66dac5f4a562842a26bc91
Christian Fries <email@christian-fries.de>
2018-11-17 19:47:12 +0100

    Reimplemented valuation using RandomVariableInterface.

--- src/main/java/net/finmath/montecarlo/assetderivativevaluation/products/DigitalOptionDeltaLikelihood.java
+++ src/main/java/net/finmath/montecarlo/assetderivativevaluation/products/DigitalOptionDeltaLikelihood.java
@@ -10,6 +10,7 @@ import net.finmath.exception.CalculationException;
 import net.finmath.montecarlo.assetderivativevaluation.AssetModelMonteCarloSimulationInterface;
 import net.finmath.montecarlo.assetderivativevaluation.BlackScholesModel;
 import net.finmath.montecarlo.assetderivativevaluation.MonteCarloAssetModel;
+import net.finmath.montecarlo.assetderivativevaluation.MonteCarloBlackScholesModel;
 import net.finmath.stochastic.RandomVariableInterface;
 import net.finmath.stochastic.Scalar;
 
@@ -40,77 +41,55 @@ public class DigitalOptionDeltaLikelihood extends AbstractAssetMonteCarloProduct
 	}
 
 	/**
-	 * Calculates the value of the option under a given model.
+	 * This method returns the value random variable of the product within the specified model, evaluated at a given evalutationTime.
+	 * Note: For a lattice this is often the value conditional to evalutationTime, for a Monte-Carlo simulation this is the (sum of) value discounted to evaluation time.
+	 * Cashflows prior evaluationTime are not considered.
 	 *
-	 * @param model A reference to a model
-	 * @return the value
+	 * @param evaluationTime The time on which this products value should be observed.
+	 * @param model The model used to price the product.
+	 * @return The random variable representing the value of the product discounted to evaluation time
 	 * @throws net.finmath.exception.CalculationException Thrown if the valuation fails, specific cause may be available via the <code>cause()</code> method.
 	 */
-	public double getValue(MonteCarloAssetModel model) throws CalculationException
-	{
+	@Override
+	public RandomVariableInterface getValue(double evaluationTime, AssetModelMonteCarloSimulationInterface model) throws CalculationException {
+
+		/*
+		 * The following valuation code requires in-depth knowledge of the model to calculate the denstiy analytically.
+		 */
 		BlackScholesModel blackScholesModel = null;
-		try {
-			blackScholesModel = (BlackScholesModel)model.getModel();
+		if(model instanceof MonteCarloAssetModel) {
+			try {
+				blackScholesModel = (BlackScholesModel)((MonteCarloAssetModel)model).getModel();
+			}
+			catch(Exception e) {}
 		}
-		catch(Exception e) {
+		else if(model instanceof MonteCarloBlackScholesModel) {
+			blackScholesModel = ((MonteCarloBlackScholesModel)model).getModel();
+		}
+		if(model == null) {
 			throw new ClassCastException("This method requires a Black-Scholes type model (MonteCarloBlackScholesModel).");
 		}
 
 		// Get underlying and numeraire
 		RandomVariableInterface underlyingAtMaturity	= model.getAssetValue(maturity,0);
-		RandomVariableInterface numeraireAtMaturity	= model.getNumeraire(maturity);
 		RandomVariableInterface underlyingAtToday		= model.getAssetValue(0.0,0);
+
+		// Get some model parameters
+		double T		= maturity-evaluationTime;
+		double r		= blackScholesModel.getRiskFreeRate().doubleValue();
+		double sigma	= blackScholesModel.getVolatility().doubleValue();
+
+		RandomVariableInterface lr = underlyingAtMaturity.log().sub(underlyingAtToday.log()).sub(r * T - 0.5 * sigma*sigma * T).div(sigma * sigma * T).div(underlyingAtToday);
+
+		RandomVariableInterface payoff = underlyingAtMaturity.sub(strike).choose(new Scalar(1.0), new Scalar(0.0));
+
+		RandomVariableInterface modifiedPayoff = payoff.mult(lr);
+
+		RandomVariableInterface numeraireAtMaturity	= model.getNumeraire(maturity);
 		RandomVariableInterface numeraireAtToday		= model.getNumeraire(0);
-		RandomVariableInterface monteCarloWeights		= model.getMonteCarloWeights(maturity);
+		RandomVariableInterface monteCarloWeightsAtMaturity		= model.getMonteCarloWeights(maturity);
+		RandomVariableInterface monteCarloWeightsAtToday		= model.getMonteCarloWeights(maturity);
 
-		/*
-		 *  The following way of calculating the expected value (average) is discouraged since it makes too strong
-		 *  assumptions on the internals of the <code>RandomVariableAccumulatorInterface</code>. Instead you should use
-		 *  the mutators sub, div, mult and the getter getAverage. This code is provided for illustrative purposes.
-		 */
-		double average = 0.0;
-		for(int path=0; path<model.getNumberOfPaths(); path++)
-		{
-			if(underlyingAtMaturity.get(path) > strike)
-			{
-				// Get some model parameters
-				double T		= maturity;
-				double S0		= underlyingAtToday.get(path);
-				double r		= blackScholesModel.getRiskFreeRate().doubleValue();
-				double sigma	= blackScholesModel.getVolatility().doubleValue();
-
-				double ST		= underlyingAtMaturity.get(path);
-
-				double x		= 1.0 / (sigma * Math.sqrt(T)) * (Math.log(ST) - (r * T - 0.5 * sigma*sigma * T + Math.log(S0)));
-
-				double lr;
-				if(isLikelihoodByFiniteDifference) {
-					double h		= 1E-6;
-
-					double x1		= 1.0 / (sigma * Math.sqrt(T)) * (Math.log(ST) - (r * T - 0.5 * sigma*sigma * T + Math.log(S0)));
-					double logPhi1	= Math.log(1.0/Math.sqrt(2 * Math.PI) * Math.exp(-x1*x1/2.0) / (ST * (sigma) * Math.sqrt(T)) );
-
-					double x2		= 1.0 / (sigma * Math.sqrt(T)) * (Math.log(ST) - (r * T - 0.5 * sigma*sigma * T + Math.log(S0+h)));
-					double logPhi2	= Math.log(1.0/Math.sqrt(2 * Math.PI) * Math.exp(-x2*x2/2.0) / (ST * (sigma) * Math.sqrt(T)) );
-
-					lr		= (logPhi2 - logPhi1) / h;
-				}
-				else {
-					lr		= x / (sigma * Math.sqrt(T)) / S0;
-				}
-
-				double payOff			= 1.0;	// Note: if condition (underlyingAtMaturity > strike) already applied
-				double modifiedPayoff	= payOff * lr;
-
-				average += modifiedPayoff / numeraireAtMaturity.get(path) * monteCarloWeights.get(path) * numeraireAtToday.get(path);
-			}
-		}
-
-		return average;
-	}
-
-	@Override
-	public RandomVariableInterface getValue(double evaluationTime, AssetModelMonteCarloSimulationInterface model) throws CalculationException {
-		return new Scalar(getValue((MonteCarloAssetModel)model));//new RuntimeException("Method not supported.");
+		return modifiedPayoff.div(numeraireAtMaturity).mult(numeraireAtToday).mult(monteCarloWeightsAtMaturity).div(monteCarloWeightsAtToday);
 	}
 }
