7fdd7fe92a4748161df5dda3267789af1f820883
Christian Fries <email@christian-fries.de>
2019-02-02 23:12:24 +0100

    Added name.

--- src/main/java/net/finmath/montecarlo/interestrate/CalibrationProduct.java
+++ src/main/java/net/finmath/montecarlo/interestrate/CalibrationProduct.java
@@ -10,28 +10,54 @@ import net.finmath.stochastic.RandomVariable;
 import net.finmath.stochastic.Scalar;
 
 /**
- * A class for calibration products, that is a tripple (P,V,w) where P is a product, V is a target value and w is a weight.
+ * A class for calibration products, that is a triple (P,V,w) where P is a product, V is a target value and w is a weight.
  *
  * @author Christian Fries
  */
 public class CalibrationProduct {
 
+	private final String 	name;
 	private final AbstractLIBORMonteCarloProduct		product;
 	private final RandomVariable				targetValue;
 	private final double								weight;
 
-	public CalibrationProduct(AbstractLIBORMonteCarloProduct product, RandomVariable targetValue, double weight) {
+	/**
+	 * Construct a calibration product. A calibration product consists of a product implementing {@link AbstractLIBORMonteCarloProduct}, a target value
+	 * given as {@link RandomVariable} and a weight. In addition you may give a short name which may be printed by the logger.
+	 * 
+	 * @param name A short name (for example a "SYMBOL" representing the product.
+	 * @param product The product.
+	 * @param targetValue The target value.
+	 * @param weight The calibration weight.
+	 */
+	public CalibrationProduct(String name, AbstractLIBORMonteCarloProduct product, RandomVariable targetValue, double weight) {
 		super();
+		this.name			= name;
 		this.product		= product;
 		this.targetValue	= targetValue;
 		this.weight			= weight;
 	}
 
+	public CalibrationProduct(String name, AbstractLIBORMonteCarloProduct product, double targetValue, double weight) {
+		this(name, product, new Scalar(targetValue), weight);
+	}
+
+	public CalibrationProduct(AbstractLIBORMonteCarloProduct product, RandomVariable targetValue, double weight) {
+		this(null, product, targetValue, weight);
+	}
+
 	public CalibrationProduct(AbstractLIBORMonteCarloProduct product, double targetValue, double weight) {
-		super();
-		this.product		= product;
-		this.targetValue	= new Scalar(targetValue);
-		this.weight			= weight;
+		this(product, new Scalar(targetValue), weight);
+	}
+
+	/**
+	 * The method returns a short name for this calibration product. If no short name was given, the method returns <code>getProduct().toString()</code>.
+	 *
+	 * @return A short name for this calibration product. If no short name was given, the method returns <code>getProduct().toString()</code>
+	 */
+	public String getName() {
+		if(name != null) return name;
+		else return product.toString();
 	}
 
 	/**
