3af95fbf208713b9fd7b3a3eecb67f81871d785f
Christian Fries <email@christian-fries.de>
2020-01-26 16:02:45 +0100

    Added JavaDoc.

--- src/main/java/net/finmath/montecarlo/RandomVariableFactory.java
+++ src/main/java/net/finmath/montecarlo/RandomVariableFactory.java
@@ -2,16 +2,58 @@ package net.finmath.montecarlo;
 
 import net.finmath.stochastic.RandomVariable;
 
+/**
+ * A factory for creating objects implementing <code>net.finmath.stochastic.RandomVariable</code>.
+ * 
+ * Use this interface in your implementations to allow <i>dependency injection</i>, i.e. to allow the use
+ * of different implementations of <code>net.finmath.stochastic.RandomVariable</code> whenever random variables
+ * need to be constructed.
+ * 
+ * @see net.finmath.stochastic.RandomVariable
+ * 
+ * @author Christian Fries
+ * @version 1.0
+ */
 public interface RandomVariableFactory {
 
+	/**
+	 * Create a (deterministic) random variable form a constant.
+	 * 
+	 * @param value A constant value.
+	 * @return The <code>RandomVariable</code>.
+	 */
 	RandomVariable createRandomVariable(double value);
 
+	/**
+	 * Create a (deterministic) random variable form a constant using a specific filtration time.
+	 * 
+	 * @param value A constant value.
+	 * @return The <code>RandomVariable</code>.
+	 */
 	RandomVariable createRandomVariable(double time, double value);
 
+	/**
+	 * Create a random variable form an array using a specific filtration time.
+	 * 
+	 * @param value Array representing values of the random variable at the sample paths.
+	 * @return The <code>RandomVariable</code>.
+	 */
 	RandomVariable createRandomVariable(double time, double[] values);
 
+	/**
+	 * Create an array of (deterministic) random variables from an array of constants.
+	 * 
+	 * @param value Array representing constants.
+	 * @return The <code>RandomVariable</code>.
+	 */
 	RandomVariable[] createRandomVariableArray(double[] values);
 
+	/**
+	 * Create a matrix of (deterministic) random variables from an matrix of constants.
+	 * 
+	 * @param value Matrix representing constants.
+	 * @return The <code>RandomVariable</code>.
+	 */
 	RandomVariable[][] createRandomVariableMatrix(double[][] values);
 
 }
