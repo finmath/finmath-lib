38b110e9579be3e9ef78a63df0ad58427c992941
Christian Fries <email@christian-fries.de>
2020-03-30 19:36:51 +0200

    Java 8 compatibility.

--- src/test/java/net/finmath/montecarlo/hybridassetinterestrate/HybridAssetLIBORModelMonteCarloSimulationFromModelsTest.java
+++ src/test/java/net/finmath/montecarlo/hybridassetinterestrate/HybridAssetLIBORModelMonteCarloSimulationFromModelsTest.java
@@ -35,7 +35,7 @@ public class HybridAssetLIBORModelMonteCarloSimulationFromModelsTest {
 		// Create a corresponding MC process
 		final MonteCarloProcessFromProcessModel process = new EulerSchemeFromProcessModel(blackScholesModel, new BrownianMotionLazyInit(model.getTimeDiscretization(), 1 /* numberOfFactors */, numberOfPaths, 323, randomVariableFactory));
 
-		final var model2 = new MonteCarloAssetModel(blackScholesModel, process);
+		final MonteCarloAssetModel model2 = new MonteCarloAssetModel(blackScholesModel, process);
 
 		final HybridAssetLIBORModelMonteCarloSimulationFromModels model3 = new HybridAssetLIBORModelMonteCarloSimulationFromModels(model, model2);
 
