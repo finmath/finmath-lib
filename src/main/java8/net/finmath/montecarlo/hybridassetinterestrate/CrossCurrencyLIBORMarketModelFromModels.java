4b6aa469c466da63df7470ef3b4becbc47d91055
Christian Fries <email@christian-fries.de>
2020-03-30 20:35:02 +0200

    Sync Java 8 with Java 11 code base.

---
+++ src/main/java8/net/finmath/montecarlo/hybridassetinterestrate/CrossCurrencyLIBORMarketModelFromModels.java
@@ -0,0 +1,140 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. Contact: email@christianfries.com.
+ *
+ * Created on 07.05.2013
+ */
+
+package net.finmath.montecarlo.hybridassetinterestrate;
+
+import java.util.Map;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.montecarlo.MonteCarloSimulationModel;
+import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationModel;
+import net.finmath.montecarlo.process.MonteCarloProcessFromProcessModel;
+import net.finmath.stochastic.RandomVariable;
+import net.finmath.stochastic.Scalar;
+import net.finmath.time.TimeDiscretization;
+
+/**
+ * Cross Currency LIBOR Market Model with Black-Scholes FX Model.
+ *
+ * @author Christian Fries
+ */
+public class CrossCurrencyLIBORMarketModelFromModels implements HybridAssetMonteCarloSimulation {
+
+	private final String										baseModel;
+
+	private final Map<String, LIBORModelMonteCarloSimulationModel>	interestRatesModels;
+	private final Map<String, MonteCarloProcessFromProcessModel> 	fxModels;
+
+	/**
+	 * Create a Cross Currency LIBOR Market Model with Black-Scholes FX Model.
+	 *
+	 * @param baseModel The name of the interest rate model used for the numeraire.
+	 * @param interestRatesModels A collection of single currency interest rate models.
+	 * @param fxModels A collection of (corresponding) fx models.
+	 */
+	public CrossCurrencyLIBORMarketModelFromModels(String baseModel,
+			Map<String, LIBORModelMonteCarloSimulationModel> interestRatesModels,
+			Map<String, MonteCarloProcessFromProcessModel> fxModels) {
+		super();
+		this.baseModel				= baseModel;
+		this.interestRatesModels	= interestRatesModels;
+		this.fxModels				= fxModels;
+	}
+
+
+	public LIBORModelMonteCarloSimulationModel getBaseModel() {
+		return interestRatesModels.get(baseModel);
+	}
+
+	@Override
+	public RandomVariable getRandomVariableForConstant(double value) {
+		return getBaseModel().getRandomVariableForConstant(value);
+	}
+
+	@Override
+	public int getNumberOfPaths() {
+		return getBaseModel().getNumberOfPaths();
+	}
+
+	@Override
+	public TimeDiscretization getTimeDiscretization() {
+		return getBaseModel().getTimeDiscretization();
+	}
+
+	@Override
+	public double getTime(int timeIndex) {
+		return getBaseModel().getTime(timeIndex);
+	}
+
+	@Override
+	public int getTimeIndex(double time) {
+		return getBaseModel().getTimeIndex(time);
+	}
+
+	@Override
+	public RandomVariable getMonteCarloWeights(int timeIndex) throws CalculationException {
+		// TODO Check for equal weights on all providers!
+		return getBaseModel().getMonteCarloWeights(timeIndex);
+	}
+
+	@Override
+	public RandomVariable getMonteCarloWeights(double time) throws CalculationException {
+		// TODO Check for equal weights on all providers!
+		return getBaseModel().getMonteCarloWeights(time);
+	}
+
+	@Override
+	public MonteCarloSimulationModel getCloneWithModifiedData(Map<String, Object> dataModified) throws CalculationException {
+		throw new UnsupportedOperationException("Method not implemented.");
+	}
+
+	@Override
+	public RandomVariable getNumeraire(double time) throws CalculationException {
+		return getBaseModel().getNumeraire(time);
+	}
+
+	@Override
+	public RandomVariable getNumeraire(String account, double time) throws CalculationException {
+		return interestRatesModels.get(account).getNumeraire(time);
+	}
+
+	@Override
+	public RandomVariable getValue(RiskFactorID riskFactorIdentifyer, double time) throws CalculationException {
+		if(riskFactorIdentifyer instanceof RiskFactorForwardRate) {
+			final RiskFactorForwardRate riskFactorForwardRate = (RiskFactorForwardRate)riskFactorIdentifyer;
+			final LIBORModelMonteCarloSimulationModel riskFactorModel = interestRatesModels.get(riskFactorIdentifyer.getName());
+
+			final RandomVariable forwardRate = riskFactorModel.getLIBOR(time,riskFactorForwardRate.getPeriodStart(), riskFactorForwardRate.getPeriodEnd());
+
+			if(!riskFactorIdentifyer.getName().equals(baseModel)) {
+				final MonteCarloProcessFromProcessModel riskFactorModelFX = fxModels.get(riskFactorIdentifyer.getName());
+				final RandomVariable fxRate = riskFactorModelFX.getProcessValue(riskFactorModelFX.getTimeIndex(time), 0);
+				//				forwardRate = forwardRate.div(fxRate);
+			}
+
+			return forwardRate;
+		}
+		else if(riskFactorIdentifyer instanceof RiskFactorFX) {
+			if(riskFactorIdentifyer.getName().equals(baseModel)) {
+				return new Scalar(1.0);
+			}
+
+			final MonteCarloProcessFromProcessModel riskFactorModelFX = fxModels.get(riskFactorIdentifyer.getName());
+			RandomVariable fxRate = riskFactorModelFX.getProcessValue(riskFactorModelFX.getTimeIndex(time), 0);
+
+			final LIBORModelMonteCarloSimulationModel riskFactorModel = interestRatesModels.get(riskFactorIdentifyer.getName());
+			fxRate = fxRate.mult(getNumeraire(time)).div(riskFactorModel.getNumeraire(time));
+			return fxRate;
+		}
+		else {
+			throw new IllegalArgumentException("Risk factor unsupported: " + riskFactorIdentifyer);
+		}
+	}
+
+	LIBORModelMonteCarloSimulationModel getInterestRateModel(String model) {
+		return interestRatesModels.get(model);
+	}
+}
--- src/main/java8/net/finmath/montecarlo/hybridassetinterestrate/HybridAssetLIBORModelMonteCarloSimulation.java
+++ src/main/java8/net/finmath/montecarlo/hybridassetinterestrate/HybridAssetLIBORModelMonteCarloSimulation.java
@@ -3,16 +3,17 @@
  *
  * Created on 03.04.2015
  */
-
 package net.finmath.montecarlo.hybridassetinterestrate;
 
+import net.finmath.montecarlo.MonteCarloSimulationModel;
 import net.finmath.montecarlo.assetderivativevaluation.AssetModelMonteCarloSimulationModel;
 import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationModel;
 
 /**
+ * Basic interface which has to be implemented by Monte Carlo models for hybrid processes.
+ *
  * @author Christian Fries
  * @version 1.0
  */
-public interface HybridAssetLIBORModelMonteCarloSimulation extends LIBORModelMonteCarloSimulationModel, AssetModelMonteCarloSimulationModel
-{
+public interface HybridAssetLIBORModelMonteCarloSimulation extends MonteCarloSimulationModel, LIBORModelMonteCarloSimulationModel, AssetModelMonteCarloSimulationModel {
 }
