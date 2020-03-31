e0b7b024a1e7d4cf844b844992ad58fae1f8d628
Christian Fries <email@christian-fries.de>
2020-03-31 20:36:37 +0200

    Fixed implementation of FDMThetaMethod and unit tests.

--- src/test/java/net/finmath/finitedifference/BlackScholesThetaTest.java
+++ src/test/java/net/finmath/finitedifference/BlackScholesThetaTest.java
@@ -2,6 +2,7 @@ package net.finmath.finitedifference;
 
 import java.util.Arrays;
 
+import net.finmath.finitedifference.products.FDMEuropeanPutOption;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,35 +15,19 @@ import net.finmath.functions.AnalyticFormulas;
 public class BlackScholesThetaTest {
 
 	@Test
-	public void test() throws AssertionError {
-		final double riskFreeRate = 0.06;
-		final double volatility = 0.4;
-		final double optionMaturity = 1;
-		final double optionStrike = 50;
+	public void testEuropeanCallOption() throws AssertionError {
+		double riskFreeRate = 0.06;
+		double volatility = 0.4;
+		double optionMaturity = 1;
+		double optionStrike = 50;
 
-		/*
-		BlackScholesTheta blackScholesFD = new BlackScholesTheta();
-		double[][] stockAndOptionPrice = blackScholesFD.solve();
-		double[] initialStockPrice = stockAndOptionPrice[0];
-		double[] optionValue = stockAndOptionPrice[1];
+		int numTimesteps = 35;
+		int numSpacesteps = 120;
+		int numStandardDeviations = 5;
+		double initialValue = 50;
+		double theta = 0.5;
 
-		double[] analyticalOptionValue = new double[stockAndOptionPrice[0].length];
-		for (int i =0; i < analyticalOptionValue.length; i++) {
-			analyticalOptionValue[i] = AnalyticFormulas.blackScholesOptionValue(initialStockPrice[i], riskFreeRate,
-					volatility, optionMaturity, optionStrike, true);
-		}
-
-		Assert.assertArrayEquals(optionValue, analyticalOptionValue, 2e-2);
-		 */
-
-		// First refactoring attempt
-		final int numTimesteps = 35;
-		final int numSpacesteps = 120;
-		final int numStandardDeviations = 5;
-		final double initialValue = 50;
-		final double theta = 0.5;
-
-		final FiniteDifference1DModel model = new FDMBlackScholesModel(
+		FiniteDifference1DModel model = new FDMBlackScholesModel(
 				numTimesteps,
 				numSpacesteps,
 				numStandardDeviations,
@@ -51,18 +36,56 @@ public class BlackScholesThetaTest {
 				initialValue,
 				riskFreeRate,
 				volatility);
+		FiniteDifference1DProduct callOption = new FDMEuropeanCallOption(optionMaturity, optionStrike);
 
-		final FiniteDifference1DProduct callOption = new FDMEuropeanCallOption(optionMaturity, optionStrike);
-		final double[][] valueFDM = callOption.getValue(0.0, model);
-		final double[] initialStockPrice = valueFDM[0];
-		final double[] optionValue = valueFDM[1];
-		final double[] analyticalOptionValue = new double[optionValue.length];
-		for (int i =0; i < analyticalOptionValue.length; i++) {
-			analyticalOptionValue[i] = AnalyticFormulas.blackScholesOptionValue(initialStockPrice[i], riskFreeRate,
+		double[][] valueCallFDM = callOption.getValue(0.0, model);
+		double[] initialStockPriceForCall = valueCallFDM[0];
+		double[] callOptionValue = valueCallFDM[1];
+		double[] analyticalCallOptionValue = new double[callOptionValue.length];
+		for (int i =0; i < analyticalCallOptionValue.length; i++) {
+			analyticalCallOptionValue[i] = AnalyticFormulas.blackScholesOptionValue(initialStockPriceForCall[i], riskFreeRate,
 					volatility, optionMaturity, optionStrike, true);
 		}
+		System.out.println(Arrays.toString(callOptionValue));
+		Assert.assertArrayEquals(callOptionValue, analyticalCallOptionValue, 1e-2);
 
-		Assert.assertArrayEquals(optionValue, analyticalOptionValue, 1.2e-2);
-		System.out.println(Arrays.toString(optionValue));
+
 	}
+
+	@Test
+	public void testEuropeanPutOption() throws AssertionError {
+		double riskFreeRate = 0.06;
+		double volatility = 0.4;
+		double optionMaturity = 1;
+		double optionStrike = 50;
+
+		int numTimesteps = 35;
+		int numSpacesteps = 120;
+		int numStandardDeviations = 5;
+		double initialValue = 50;
+		double theta = 0.5;
+
+		FiniteDifference1DModel model = new FDMBlackScholesModel(
+				numTimesteps,
+				numSpacesteps,
+				numStandardDeviations,
+				optionStrike, // center of the grid.
+				theta,
+				initialValue,
+				riskFreeRate,
+				volatility);
+		FiniteDifference1DProduct putOption = new FDMEuropeanPutOption(optionMaturity, optionStrike);
+		double[][] valuePutFDM = putOption.getValue(0.0, model);
+		double[] initialStockPriceForPut = valuePutFDM[0];
+		double[] putOptionValue = valuePutFDM[1];
+		double[] analyticalPutOptionValue = new double[putOptionValue.length];
+		for (int i =0; i < analyticalPutOptionValue.length; i++) {
+			analyticalPutOptionValue[i] = AnalyticFormulas.blackScholesOptionValue(initialStockPriceForPut[i], riskFreeRate,
+					volatility, optionMaturity, optionStrike, false);
+		}
+		System.out.println(Arrays.toString(putOptionValue));
+		Assert.assertArrayEquals(putOptionValue, analyticalPutOptionValue, 1e-2);
+	}
+
+
 }
