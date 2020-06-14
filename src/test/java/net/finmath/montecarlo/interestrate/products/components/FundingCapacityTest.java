74af3066062546dfe93c8fbbe991be76bac9616e
Christian Fries <email@christian-fries.de>
2020-06-14 14:28:15 +0200

    First implementation of FundingCapacity.

---
+++ src/test/java/net/finmath/montecarlo/interestrate/products/components/FundingCapacityTest.java
@@ -0,0 +1,41 @@
+package net.finmath.montecarlo.interestrate.products.components;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import net.finmath.stochastic.RandomVariable;
+import net.finmath.stochastic.Scalar;
+
+class FundingCapacityTest {
+
+	@Test
+	void test() {
+
+		SortedMap<Double, Double> instSurvProb = new TreeMap<Double, Double>();
+		instSurvProb.put(1.5, 0.9);
+		instSurvProb.put(2.0, 0.8);
+		instSurvProb.put(5.0, 0.7);
+		instSurvProb.put(10.0, 0.5);
+
+		FundingCapacity fc = new FundingCapacity("EUR", new Scalar(0.0), instSurvProb);
+
+		RandomVariable sp = fc.getSurvivalProbabilityRequiredFunding(1.0, new Scalar(1.0));
+		Assertions.assertEquals(0.9, sp.doubleValue());
+
+		RandomVariable sp2 = fc.getSurvivalProbabilityRequiredFunding(1.0, new Scalar(1.0));
+		Assertions.assertEquals(0.85, sp2.doubleValue(), 1E-12);
+
+		RandomVariable sp3 = fc.getSurvivalProbabilityRequiredFunding(1.0, new Scalar(3.0));
+		Assertions.assertEquals(0.7, sp3.doubleValue(), 1E-12);
+
+		RandomVariable sp4 = fc.getSurvivalProbabilityRequiredFunding(1.0, new Scalar(-4.0));
+		Assertions.assertEquals((3*0.7+0.5*0.8+0.5*0.9)/4, sp4.doubleValue(), 1E-12);
+
+		RandomVariable level = fc.getCurrentFundingLevel();
+		Assertions.assertEquals(1.0, level.doubleValue(), 1E-12);
+	}
+
+}
