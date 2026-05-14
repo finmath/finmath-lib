c3d7a25e5a993437732e2e30775d2c4425dba91f
Christian Fries <email@christian-fries.de>
2026-05-14 10:35:28 +0200

    Adapting deploy process of java-8 artefact to new maven central deploy plugin.

--- maven-release.sh
+++ maven-release.sh
@@ -1,67 +1,78 @@
-# This script performs a release of finmath-lib. The release is pushed to Maven central.
+#!/usr/bin/env bash
+
+# This script performs a release of finmath-lib and publishes one Maven Central
+# deployment containing the Java 11 main artifact, Java 11 sources/javadocs,
+# and the Java 8 classifier artifact.
 #
-#
-# While a replease can be performed with just the two lines
-#
-#   mvn release:prepare
-#   mvn release:perform
-#
-# the script performs some additional task in order to:
-# - deploy other profiles with Maven classifiers (java8)
-# - run the clover report, which can be run for java-8 only.
-#
+# The important change compared to the old OSSRH workflow is that we no longer
+# run deploy multiple times. The Central Portal deploy plugin creates one
+# deployment/bundle per deploy run, so all artifacts must be attached before the
+# single final deploy.
 #
 # Before running this script you should check the profiles via
-# 	mvn clean javadoc:javadoc checkstyle:check test -P java-8.
+# 	mvn clean javadoc:javadoc checkstyle:check test -P=java-8,-java-11
 # and
-# 	mvn javadoc:javadoc checkstyle:check test
-#
-# To create a new release you need to run
-# 	mvn release:prepare
-# before running this script and specify the version and check if this is successful.
-#
-# To re-release a given tag / version you need to run
-# 	git checkout finmath-lib-<verion>
-# before running this script.
+# 	mvn clean javadoc:javadoc checkstyle:check test -P=java-11,-java-8
 #
 # If this script is run on a newly setup system, the following things need to be setup:
-# - .m2/settings.xml - has to contain the passwords for sonatype, github and finmath.net site
+# - .m2/settings.xml has to contain the passwords/tokens for central, github and the site
 # - .keyring has to contain the gpg keys
-# - a gpg app has be installed (referenced in the .m2/settings.mxl)
-# - you have to run mvn site:deploy manually once to get prompted for adding the RSA fingerprint
+# - a gpg app has to be installed and configured
+# - you may have to run mvn site:deploy manually once to get prompted for adding the RSA fingerprint
 #
-#
-# The code coverage report from clover currently only works for the Java 8 branch.
-# Hence there is an additional run of mvn clover:instrument with -P java-8
-# To prevent the deletion of the report(s), this run must not use mvn clean.
-# Since we switch the SDK, we have to do a recompilation via
-# -Dmaven.compiler.useIncrementalCompilation=false
-#
-#
+# This script assumes that the final deploy is run with a Java 11+ JDK. The
+# Java 8 classifier artifact is first built separately, copied outside target,
+# and then attached to the final Java 11 deploy.
 
+set -euo pipefail
 set -o verbose
 
-#
-# Run mvn release:prepare manually to check if the none of the tests fail
-#
-
+# Run mvn release:prepare explicitly so that you can check that release
+# preparation is successful before the final Central deployment.
 mvn release:prepare
 
-#
+# Let release:perform check out the release tag into target/checkout and verify
+# it, but do not deploy from release:perform. The final deploy below attaches all
+# artifacts first and then publishes once.
+echo "###################"
+echo "# RELEASE:PERFORM #"
+echo "###################"
 
-# relase the default profile
-echo ###################
-echo # RELEASE:PERFORM #
-echo ###################
+mvn release:perform \
+	-Dgoals=verify \
+	-Darguments="-DskipTests -Dgpg.skip=true -Dmaven.javadoc.skip=true -Dmaven.source.skip=true"
 
-mvn release:perform
-
-# deploy the other profiles (we do this skipping tests) (the clean is important here!)
 cd target/checkout/
-mvn clean verify javadoc:jar source:jar gpg:sign deploy -P java-8 -D skipTests
 
-# re-deploy the java-11 profile to deploy the right source and javadoc files to Maven central
-mvn clean verify javadoc:jar source:jar gpg:sign deploy -P java-11 -D skipTests
+VERSION="$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)"
+ATTACH_DIR="$(pwd)/../release-artifacts"
+JAVA8_ARTIFACT="${ATTACH_DIR}/finmath-lib-${VERSION}-java8.jar"
 
-echo Turn to http://oss.sonatype.org to release the artifact.
-echo Then release the new site.
+rm -rf "${ATTACH_DIR}"
+mkdir -p "${ATTACH_DIR}"
+
+# Build the Java 8 classifier jar, but do not deploy it. Sources and javadocs
+# are intentionally skipped here because they should come from the Java 11 build.
+mvn clean package \
+	-P=java-8,-java-11 \
+	-DskipTests \
+	-Dmaven.javadoc.skip=true \
+	-Dmaven.source.skip=true
+
+if [ ! -f "target/finmath-lib-${VERSION}-java8.jar" ]; then
+	echo "ERROR: Expected Java 8 artifact target/finmath-lib-${VERSION}-java8.jar was not created." >&2
+	exit 1
+fi
+
+cp "target/finmath-lib-${VERSION}-java8.jar" "${JAVA8_ARTIFACT}"
+
+# Final deploy: build the Java 11 main artifact, attach Java 11 sources/javadocs
+# and the pre-built Java 8 classifier jar, sign everything, and create exactly
+# one Central Portal deployment.
+mvn clean deploy \
+	-P=java-11,attach-java8-artifact,-java-8 \
+	-Djava8.artifact="${JAVA8_ARTIFACT}" \
+	-DskipTests
+
+echo "Turn to https://central.sonatype.com/publishing/deployments to publish/release the deployment."
+echo "Then release the new site."
