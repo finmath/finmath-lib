2763b337743e8b5e378a57c602d68ec3be8b9efd
Christian Fries <email@christian-fries.de>
2026-05-19 09:30:11 +0200

    Fixed site deploy issue related to git buffer.

--- maven-release-site.sh
+++ maven-release-site.sh
@@ -1,4 +1,13 @@
+#!/bin/sh
+set -e
 
 # deploy site (clover:instrument takes a long time)
-mvn compile clover:instrument -P java-8 -Dmaven.compiler.useIncrementalCompilation=false
-mvn install site site:stage site-deploy -Dmaven.compiler.useIncrementalCompilation=false
+#mvn compile clover:instrument -P java-8 -Dmaven.compiler.useIncrementalCompilation=false
+
+# need to limit buffer size to avoid https issues because our pages have become big
+GIT_CONFIG_COUNT=2 \
+GIT_CONFIG_KEY_0=http.https://github.com/.version \
+GIT_CONFIG_VALUE_0=HTTP/1.1 \
+GIT_CONFIG_KEY_1=http.https://github.com/.postBuffer \
+GIT_CONFIG_VALUE_1=52428800 \
+mvn install site site:stage site-deploy -DskipTests "$@"
